3baacd35e26ba744d1c1591cb1e28622798be0d1
TimonKnigge <timonknigge@live.nl>
2015-02-16 23:33:14 +0100

    Adds Dilworth's, Mirsky's theorem, updates bellmann ford.

--- snippets/graphs/bellmannford.cpp
+++ snippets/graphs/bellmannford.cpp
@@ -1,7 +1,7 @@
 // Edge list as in with Dijkstra's (see above)
 void bellmann_ford(vvii edges, int source, vi &dist) {
     dist.assign(edges.size(), INF); dist[source] = 0;
-    for (int iter = 0; iter < edges.size(); ++iter)
+    for (int iter = 0; iter < edges.size() - 1; ++iter)
         for (int u = 0; u < edges.size(); ++u)
             for (ii e : edges[u])
                 dist[e.first] = min(dist[e.first], dist[u] + e.second);
