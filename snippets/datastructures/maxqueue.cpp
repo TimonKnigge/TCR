b3ce481cfa83b2a8aae33e7e7c4141476cb372a1
TimonKnigge <timonknigge@live.nl>
2015-02-16 20:18:55 +0100

    Adds the max queue.

---
+++ snippets/datastructures/maxqueue.cpp
@@ -0,0 +1,35 @@
+template <class T>
+class MaxQueue {
+public:
+	stack< pair<T, T> > inbox, outbox;
+	void enqueue(T val) {
+		T m = val;
+		if (!inbox.empty()) m = max(m, inbox.top().second);
+		inbox.push(pair<T, T>(val, m));
+	}
+	bool dequeue(T* d = nullptr) {
+		if (outbox.empty() && !inbox.empty()) {
+			pair<T, T> t = inbox.top(); inbox.pop();
+			outbox.push(pair<T, T>(t.first, t.first));
+			while (!inbox.empty()) {
+				t = inbox.top(); inbox.pop();
+				T m = max(t.first, outbox.top().second);
+				outbox.push(pair<T, T>(t.first, m));
+			}
+		}
+		if (outbox.empty()) return false;
+		else {
+			if (d != nullptr) *d = outbox.top().first;
+			outbox.pop();
+			return true;
+		}
+	}
+	bool empty() { return outbox.empty() && inbox.empty(); }
+	size_t size() { return outbox.size() + inbox.size(); }
+	T get_max() {
+		if (!outbox.empty() && !inbox.empty())
+			return max(outbox.top().second, inbox.top().second);
+		if (outbox.empty()) return inbox.top().second;
+		return outbox.top().second;
+	}
+};
