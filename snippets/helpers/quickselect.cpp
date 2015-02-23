bbb5de45cc3ebee8f9d6efd9d7115a9c4376bf9f
unknown <4164806@UU045604.soliscom.uu.nl>
2015-02-23 12:24:25 +0100

    Adds QuickSelect.

---
+++ snippets/helpers/quickselect.cpp
@@ -0,0 +1,16 @@
+template<class T> 
+T quickselect(vector<T>& v, int l, int r, int k) {
+    int p = l + (rand() % (r - l));
+    swap(v[l], v[p]); 
+    bool alt = false; p = l + 1;
+    for (int j = l + 1; j < r; ++j) {
+        if (alt = !alt) {
+               if (v[j] <  v[l]) swap(v[p++], v[j]);
+        } else if (v[j] <= v[l]) swap(v[p++], v[j]);
+    }
+    swap(v[l], v[--p]);
+    
+    if (p == k) return v[k];
+    if (p >  k) return quickselect(v, l, p, k);
+    if (p <  k) return quickselect(v, p + 1, r, k);
+}
