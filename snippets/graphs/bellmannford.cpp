7272c58ca94ebcb4c93d63fae40d77d81072650f
Timon Knigge <timonknigge@live.nl>
2015-04-18 22:32:58 +0200

    Updates Bellmann-Ford to automatically report cycles.

--- snippets/graphs/bellmannford.cpp
+++ snippets/graphs/bellmannford.cpp
@@ -1,8 +1,13 @@
 // Edge list as in with Dijkstra's (see above)
-void bellmann_ford(vvii edges, int source, vi &dist) {
+bool bellmann_ford(vvii edges, int source, vi &dist) {
     dist.assign(edges.size(), INF); dist[source] = 0;
     for (int iter = 0; iter < edges.size() - 1; ++iter)
         for (int u = 0; u < edges.size(); ++u)
             for (ii e : edges[u])
                 dist[e.first] = min(dist[e.first], dist[u] + e.second);
+    for (int u = 0; u < edges.size(); ++u)
+        for (ii e : edges[u])
+            if (dist[e.first] > dist[u] + e.second)
+                return false;
+    return true;
 }
