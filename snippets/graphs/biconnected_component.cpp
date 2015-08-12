0b737705285c65b4d15f1fc3b8167ee5827543db
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2015-08-12 18:31:03 +0200

    Add biconnected components; clean lib_ragnar/graph

---
+++ snippets/graphs/biconnected_component.cpp
@@ -0,0 +1,30 @@
+#include "../header.h"
+struct BCC{		// find AVs and bridges in an undirected graph
+	vvi& edges;
+	int V, counter = 0, root, rcs;		// root and # children of root
+	vi n,l;								// nodes,low
+	stack<int> s;
+	BCC(vvi& e) : edges(e), V(e.size()), n(V,-1), l(V,-1) {}
+	void visit(int u, int p) {			// also pass the parent
+		l[u] = n[u] = counter++; s.push(u);
+		for(auto& v : edges[u]){
+			if (n[v] == -1) {
+				if (u == root) rcs++; visit(v,u);
+				if (l[v] >= n[u]) {}	// u is an articulation point
+				if (l[v] > n[u]) {		// u<->v is a bridge
+					while(true){		// biconnected component
+						int w = s.top(); s.pop();	// <= ACT HERE
+						if(w==v) break;
+					}
+				}
+				l[u] = min(l[u], l[v]);
+			} else if (v != p) l[u] = min(l[u], n[v]);
+		}
+	}
+	void run() {
+		REP(u,V) if (n[u] == -1) {
+			root = u; rcs = 0; visit(u,-1);
+			if(rcs > 1) {}				// u is articulation point
+		}
+	}
+};
