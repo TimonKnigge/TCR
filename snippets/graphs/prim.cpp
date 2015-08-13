2af824dc57b055e3c5d2df6d46188bd723169316
Timon Knigge <TimonKnigge@users.noreply.github.com>
2015-08-13 17:24:48 +0200

    Fixes prim.

--- snippets/graphs/prim.cpp
+++ snippets/graphs/prim.cpp
@@ -1,7 +1,8 @@
 #include "../header.h"
-struct AdjEdge { int v, weight; };	// adjacency list edge
+struct AdjEdge { int v; ll weight; };	// adjacency list edge
 struct Edge { int u, v; };			// edge u->v for output
-struct PQ { int weight; Edge e; };	// PQ element
+struct PQ { ll weight; Edge e; };	// PQ element
+bool operator>(const PQ &lhs, const PQ &rhs) { return lhs.weight > rhs.weight; }
 ll prim(vector<vector<AdjEdge>>& adj, vector<Edge>& tree) {
 	ll tc = 0; vb intree(adj.size(), false);
 	priority_queue<PQ, vector<PQ>, greater<PQ> > pq;
@@ -9,7 +10,7 @@ ll prim(vector<vector<AdjEdge>>& adj, vector<Edge>& tree) {
 	for (auto& e : adj[0]) pq.push({e.weight, {0, e.v}});
 	while (!pq.empty()) {
 		auto& top = pq.top();
-		int c = top.weight; auto e = top.e; pq.pop();
+		ll c = top.weight; auto e = top.e; pq.pop();
 		if (intree[e.v]) continue;
 		intree[e.v] = true; tc += c; tree.push_back(e);
 		for (auto& e2 : adj[e.v])
