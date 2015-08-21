b17b3d251e3aa1c939dec8443a43209b1edd3652
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2015-08-22 01:03:57 +0200

    bufgix in dijkstra

--- snippets/graphs/dijkstra.cpp
+++ snippets/graphs/dijkstra.cpp
@@ -6,8 +6,7 @@ int dijkstra(vector<vector<Edge>>& edges, int s, int t) {
 	priority_queue<PQ,vector<PQ>,greater<PQ>> pq;
 	dist[s] = 0; pq.push({0, s});
 	while (!pq.empty()) { 
-		auto& top = pq.top(); pq.pop();
-		auto d = top.d, u = top.v;
+		auto d = pq.top().d, u = pq.top().v; pq.pop();
 		if(u==t) break;			// target reached
 		if (d == dist[u])
 			for(auto& e : edges[u]) if (dist[e.v] > d + e.weight)
