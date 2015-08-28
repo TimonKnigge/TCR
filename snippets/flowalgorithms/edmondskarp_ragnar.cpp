11ea535411aa10e1d9eb9e5766b23f43f00c7d7b
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2015-08-28 22:15:14 +0200

    uniform edmondskarp constructor

--- snippets/flowalgorithms/edmondskarp_ragnar.cpp
+++ snippets/flowalgorithms/edmondskarp_ragnar.cpp
@@ -1,8 +1,8 @@
 #include "flowgraph.cpp"
 struct Edmonds_Karp{
 	FlowGraph &edges; int V,s,t; // directed graph
-	Edmonds_Karp(FlowGraph &edges, int V, int s, int t) :
-		edges(edges), V(V), s(s), t(t) {}
+	Edmonds_Karp(FlowGraph &edges, int s, int t) :
+		edges(edges), V(edges.size()), s(s), t(t) {}
 	int run() {
 		int maxflow = 0;
 		bool flowfound=true;
