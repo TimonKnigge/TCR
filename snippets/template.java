4b6f81e8e4dcd736750bd1ed8f00465b9d2dc539
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2015-08-30 21:18:34 +0200

    bugfixes java template

--- snippets/template.java
+++ snippets/template.java
@@ -1,10 +1,11 @@
 import java.io.OutputStream;
 import java.io.InputStream;
 import java.io.PrintWriter;
-import java.util.StringTokenizer
+import java.util.StringTokenizer;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.InputStream;
+import java.io.IOException;
 
 import java.util.Arrays;
 import java.math.BigInteger;
