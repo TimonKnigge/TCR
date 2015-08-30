9ddc97d8715abd31266f19c73462589bad672c95
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2015-08-30 20:39:22 +0200

    add lca to heavylight; remove old lca file

--- snippets/graphs/heavylight.cpp
+++ snippets/graphs/heavylight.cpp
@@ -1,9 +1,9 @@
 #include "../header.h"
-struct HVL {
-	int V,T; vvi &childs;	// Size; dfs-time; childs of input tree
+struct HLD {
+	int V,T; vi &p; vvi &childs;	// Size; dfs-time; input parent/childs
 	vi pr, size, heavy;		// path-root; size of subtrees; heavy child
 	vi t_in, t_out;			// dfs in and out times
-	HVL(vvi &childs) : V(childs.size()), T(0), childs(childs),
+	HLD(vvi &childs, vi &p) : V(p.size()), T(0), p(p), childs(childs),
 	pr(V,-1), size(V,-1), heavy(V,-1), t_in(V,-1), t_out(V,-1) {
 		dfs(0); set_pr(0,-1);
 	}
@@ -16,8 +16,16 @@ struct HVL {
 		}
 		heavy[u] = mi; t_out[u] = T++; return size[u];
 	}
-	void set_pr(int u, int r){	// node, path root
+	void set_pr(int u, int r){		// node, path root
 		pr[u] = r;
 		for(auto &v : childs[u]) set_pr(v, heavy[u] == v ? r : v);
+	}
+	bool is_parent(int p, int u){	// test whether p is a parent of u
+		return t_in[p] <= t_in[u] && t_out[p] >= t_out[u];
+	}
+	int lca(int u, int v){
+		while(!is_parent(pr[v],u)) v = p[pr[v]];
+		while(!is_parent(pr[u],v)) u = p[pr[u]];
+		return is_parent(u,v) ? u : v;
 	}
 };
