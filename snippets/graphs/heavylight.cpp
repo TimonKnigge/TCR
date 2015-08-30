e4776e2b22ba398fcad30d64a8c12d51c42c254c
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2015-08-30 20:28:55 +0200

    add dfs in/out times to heavylight

--- snippets/graphs/heavylight.cpp
+++ snippets/graphs/heavylight.cpp
@@ -1,30 +1,23 @@
 #include "../header.h"
 struct HVL {
-	int V;
-	vi &p;			// parents
-	vvi &childs;	// childs
-	vi skip;		// the index of the root of the current path
-	vi size;		// size of subtrees
-	vi heavy;		// heavy child
-	HVL(vvi &childs, vi &p) :
-		V(p.size()), p(p), childs(childs),
-		skip(V,-1), size(V,-1), heavy(V,-1) {
-		dfs(0);		// 0 is the root
-		set_skip(0,-1);
+	int V,T; vvi &childs;	// Size; dfs-time; childs of input tree
+	vi pr, size, heavy;		// path-root; size of subtrees; heavy child
+	vi t_in, t_out;			// dfs in and out times
+	HVL(vvi &childs) : V(childs.size()), T(0), childs(childs),
+	pr(V,-1), size(V,-1), heavy(V,-1), t_in(V,-1), t_out(V,-1) {
+		dfs(0); set_pr(0,-1);
 	}
 	int dfs(int u){
-		size[u] = 1;
+		size[u] = 1; t_in[u] = T++;
 		int m = -1, mi = -1, s;		// max, max index, size of subtree
 		for(auto &v : childs[u]){
 			size[u] += s = dfs(v);
 			if(s > m) m=s, mi = v;
 		}
-		// make heavy edge to largest child
-		heavy[u] = mi;
-		return size[u];
+		heavy[u] = mi; t_out[u] = T++; return size[u];
 	}
-	void set_skip(int u, int pr){ // node, path root
-		skip[u] = pr;
-		for(auto &v : childs[u]) set_skip(v, heavy[u] == v ? pr : v);
+	void set_pr(int u, int r){	// node, path root
+		pr[u] = r;
+		for(auto &v : childs[u]) set_pr(v, heavy[u] == v ? r : v);
 	}
 };
