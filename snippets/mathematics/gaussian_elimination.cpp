a7a3c24d387cb848efb3144635202ab76bd1dde5
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2015-08-31 18:39:20 +0200

    update Gaussian Elimination

--- snippets/mathematics/gaussian_elimination.cpp
+++ snippets/mathematics/gaussian_elimination.cpp
@@ -1,18 +1,13 @@
 #include "../header.h"
-
 template<int N> // solve x*M=a
 array<double,N> GaussianElimination(array<array<double,N>,N> &M, array<double,N> &a) {
-	int i, j, k, l; double t;
-
 	REP(i,N-1) {// eleminate rows
-		/*
-		l = i; // l will be the row with max value in col i
-		for(auto j = i+1; j < N; ++j)
+		int l = i; // l will be the row with max value in col i
+		for(auto j = i+1; j < N; ++j) // swap rows to minimize fp error
 			if (abs(M[j][i]) > abs(M[l][i]))
 				l = j;
-		// swap this pivot row to minimize floating point error
+		if(M[l][i]==0) continue;	// al values are already zero
 		swap_ranges(M[i].begin()+i,M[i].end(),M[l].begin()+i);
-		*/
 		for(auto j = i+1; j < N+1; ++j){// sweep the rows below
 			for(auto k = i+1; k < N; ++k)
 				M[j][k] -= M[i][k] * M[j][i] / M[i][i];
@@ -20,12 +15,10 @@ array<double,N> GaussianElimination(array<array<double,N>,N> &M, array<double,N>
 			M[j][i] = 0;
 		}
 	}
-
-	array<double,N> Ans; // the back substitution phase
+	array<double,N> ans; // the back substitution phase
 	for(auto j = N-1; j >= 0; --j){
-		t=0;
-		for(auto k = j+1; k < N; ++k) t += M[j][k] * Ans[k];
-		Ans[j] = (M[j][N] - t) / M[j][j]; // the answer is here
+		double t=0; for(auto k = j+1; k < N; ++k) t += M[j][k] * ans[k];
+		ans[j] = (a[j] - t) / M[j][j]; // the answer is here
 	}
-	return Ans;
+	return ans;
 }
