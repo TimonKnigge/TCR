4e4fa22343fad68251351e6a1c3005e7305cda04
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2015-09-01 22:41:56 +0200

    remove duplicate flow algorithms from .tex/.pdf

--- snippets/mathematics/reduced_row_echelon.cpp
+++ snippets/mathematics/reduced_row_echelon.cpp
@@ -4,34 +4,31 @@ using   T = double;
 using  AT = array<T,SIZE>;
 using AAT = array<array<T,SIZE+1>,SIZE>;
 constexpr T EPS = 1e-8;
-bool mult, incost;		// true when multiple or no solutions
-AT ReducedRowEchelonForm(AAT &M, int N) { // N*(N+1) matrix
-	mult = false, incost = false; int r = 0;
+bool mult, inconst;		// true when multiple or no solutions
+void ReducedRowEchelonForm(AAT &M, int N) { // N*(N+1) matrix
+	mult = false, inconst = false; int r = 0;
 	for(int c = 0; c < N+1 && r < N; c++) {
 		int j = r;
 		for(int i = r+1; i < N; i++) if(abs(M[i][c]) > abs(M[j][c])) j = i;
 		if(abs(M[j][c]) < EPS) continue;
 		swap(M[j], M[r]);
 		auto s = 1.0 / M[r][c];
-		for(int j = 0; j < N+1; j++) M[r][j] *= s;
-		for(int i = 0; i < N; i++) if (i != r) {
-			auto t = M[i][c];
-			for(int j = 0; j < N+1; j++) M[i][j] -= t * M[r][j];
-		}
+		REP(j,N+1) M[r][j] *= s;
+		REP(i,N) if (i != r) REP(j,N+1) M[i][j] -= M[i][c] * M[r][j];
 		++r;
 	}
+}
+
 	AT ans; // the back substitution phase
 	for(auto j = N-1; j >= 0; --j){
 		bool allzero=true; long double t=0;
 		for(auto k = j+1; k < N; ++k){
-			t += M[j][k] * ans[k];
-			if(abs(M[j][k])>EPS) allzero=false;
+			t += M[j][k] * ans[k]; if(abs(M[j][k])>EPS) allzero=false;
 		}
 		if(abs(M[j][j])<EPS){
-			if(allzero && abs(M[j][N]) > EPS) mult = false, incost = true;
+			if(allzero && abs(M[j][N]) > EPS) mult = false, inconst = true;
 			else mult = true;
-		} else
-			ans[j] = (M[j][N] - t) / M[j][j]; // the answer is here
+		} else ans[j] = (M[j][N] - t) / M[j][j]; // the answer is here
 	}
 	return ans;
 }
