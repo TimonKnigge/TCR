c4e4385c7e235858fdacbe54d6aaccf300f63966
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2015-09-01 22:46:00 +0200

    dinic int->long long

--- snippets/flowalgorithms/dinic_ragnar.cpp
+++ snippets/flowalgorithms/dinic_ragnar.cpp
@@ -4,18 +4,18 @@ struct Dinic{
 	vi l; vector<vector<S>::iterator> its; // levels and iterators
 	Dinic(FlowGraph &edges, int s, int t) :
 		edges(edges), V(edges.size()), s(s), t(t), l(V,-1), its(V) {}
-	int augment(int u, ll c) { // we reuse the same iterators
+	ll augment(int u, ll c) { // we reuse the same iterators
 		if (u == t) return c;
 		for(auto &i = its[u]; i != edges[u].end(); i++){
 			auto &e = *i;
 			if (e.cap > e.f && l[u] < l[e.v]) {
-				int d = augment(e.v, min(c, e.cap - e.f));
+				auto d = augment(e.v, min(c, e.cap - e.f));
 				if (d > 0) { e.f += d; edges[e.v][e.r].f -= d; return d; }
 			}	}
 		return 0;
 	}
-	int run() {
-		int flow = 0, f;
+	ll run() {
+		ll flow = 0, f;
 		while(true) {
 			fill(F(l),-1); l[s]=0; // recalculate the layers
 			queue<int> q; q.push(s);
