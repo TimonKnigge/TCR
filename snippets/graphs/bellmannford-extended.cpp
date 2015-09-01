1da291d824695e807b08f5dd8792415fa1f68fb6
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2015-09-01 23:10:09 +0200

    add comments to bellmann ford

--- snippets/graphs/bellmannford-extended.cpp
+++ snippets/graphs/bellmannford-extended.cpp
@@ -1,6 +1,6 @@
 void bellmann_ford_extended(vvii &e, int source, vi &dist, vb &cyc) {
 	dist.assign(e.size(), INF);
-	cyc.assign(e.size(), false);
+	cyc.assign(e.size(), false); // true when u is in a <0 cycle
 	dist[source] = 0;
 	for (int iter = 0; iter < e.size() - 1; ++iter){
 		bool relax = false;
@@ -12,15 +12,15 @@ void bellmann_ford_extended(vvii &e, int source, vi &dist, vb &cyc) {
 		if(!relax) break;
 	}
 	bool ch = true;
-	while (ch) {
-		ch = false;
+	while (ch) {				// keep going untill no more changes
+		ch = false;				// set dist to -INF when in cycle
 		for (int u = 0; u < e.size(); ++u)
 			if (dist[u] == INF) continue;
 			else for (auto &e : e[u])
 				if (dist[e.first] > dist[u] + e.second
 					&& !cyc[e.first]) {
 					dist[e.first] = -INF;
-					ch = true;
+					ch = true;		//return true for cycle detection only
 					cyc[e.first] = true;
 				}
 	}
