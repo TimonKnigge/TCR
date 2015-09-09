0a57d6ebc3665c45801d418fd2ddb5c90602d228
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2015-09-10 00:27:53 +0200

    fix matrix bugs

--- snippets/matrix/reduced_row_echelon.cpp
+++ snippets/matrix/reduced_row_echelon.cpp
@@ -1,36 +1,36 @@
 #include "../header.h"
-using T = double;						// type
-template<int R, int C>
-using M = array<array<T,C>,R>;	// matrix
+using T = double;
 constexpr T EPS = 1e-8;
 template<int R, int C>
-void ReducedRowEchelonForm(M<R,C> &m) {
-	int r = 0;
-	for(int c = 0; c < C && r < R; c++) {
-		int j = r;
-		for(int i = r+1; i < R; i++) if(abs(m[i][c]) > abs(m[j][c])) j = i;
-		if(abs(m[j][c]) < EPS) continue;
-		swap(m[j], m[r]);
-		auto s = 1.0 / m[r][c];
-		REP(j,C) m[r][j] *= s;
-		REP(i,R) if (i != r) REP(j,C) m[i][j] -= m[i][c] * m[r][j];
+using M = array<array<T,C>,R>;	// matrix
+template<int R, int C>
+T ReducedRowEchelonForm(M<R,C> &m, int rows) {	// return the determinant
+	int r = 0; T det = 1;							// MODIFIES the input
+	for(int c = 0; c < rows && r < rows; c++) {
+		int p = r;
+		for(int i=r+1; i<rows; i++) if(abs(m[i][c]) > abs(m[p][c])) p=i;
+		if(abs(m[p][c]) < EPS) continue;
+		swap(m[p], m[r]);		det *= ( (p-r)%2 ? -1 : 1 );
+		T s = 1.0 / m[r][c], t;	det *= m[r][c];
+		REP(j,C) m[r][j] *= s;				// make leading term in row 1
+		REP(i,rows) if (i!=r){ t = m[i][c]; REP(j,C) m[i][j] -= t*m[r][j]; }
 		++r;
 	}
+	return det;
 }
-
 bool error, inconst;	// error => multiple or inconsistent
-template<int R, int C>	// Mx = a; M:R*R, v:R*C => x:R*C
-M<R,C> solve(const M<R,R> &m, const M<R,C> &a){
+template<int R,int C>	// Mx = a; M:R*R, v:R*C => x:R*C
+M<R,C> solve(const M<R,R> &m, const M<R,C> &a, int rows){
 	M<R,R+C> q;
-	REP(r,R){
-		REP(c,R) q[r][c] = m[r][c];
-		REP(c,R) q[r][R+c] = a[r][c];
+	REP(r,rows){
+		REP(c,rows) q[r][c] = m[r][c];
+		REP(c,C) q[r][R+c] = a[r][c];
 	}
-	ReducedRowEchelonForm(q);
+	ReducedRowEchelonForm<R,R+C>(q,rows);
 	M<R,C> sol; error = false, inconst = false; 
-	REP(c,C) for(auto j = R-1; j >= 0; --j){
+	REP(c,C) for(auto j = rows-1; j >= 0; --j){
 		T t=0; bool allzero=true;
-		for(auto k = j+1; k < R; ++k)
+		for(auto k = j+1; k < rows; ++k)
 			t += q[j][k]*sol[k][c], allzero &= abs(q[j][k]) < EPS;
 		if(abs(q[j][j]) < EPS)
 			error = true, inconst |= allzero && abs(q[j][R+c]) > EPS;
@@ -38,3 +38,4 @@ M<R,C> solve(const M<R,R> &m, const M<R,C> &a){
 	}
 	return sol;
 }
+
