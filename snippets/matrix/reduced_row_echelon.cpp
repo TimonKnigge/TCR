6898f5070f1d85d66ed97ed582b3b8fb454980ee
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2015-09-09 23:28:10 +0200

    rewrite of matrix solver

--- snippets/matrix/reduced_row_echelon.cpp
+++ snippets/matrix/reduced_row_echelon.cpp
@@ -1,34 +1,40 @@
 #include "../header.h"
-constexpr int SIZE = 100;
 using T = double;						// type
-using V = array<T,SIZE>;				// vector
-using M = array<array<T,SIZE+1>,SIZE>;	// matrix
+template<int R, int C>
+using M = array<array<T,C>,R>;	// matrix
 constexpr T EPS = 1e-8;
-bool mult, inconst;		// true when multiple or no solutions
-void ReducedRowEchelonForm(M &M, int N) { // N*(N+1) matrix
-	mult = false, inconst = false; int r = 0;
-	for(int c = 0; c < N+1 && r < N; c++) {
+template<int R, int C>
+void ReducedRowEchelonForm(M<R,C> &m) {
+	int r = 0;
+	for(int c = 0; c < C && r < R; c++) {
 		int j = r;
-		for(int i = r+1; i < N; i++) if(abs(M[i][c]) > abs(M[j][c])) j = i;
-		if(abs(M[j][c]) < EPS) continue;
-		swap(M[j], M[r]);
-		auto s = 1.0 / M[r][c];
-		REP(j,N+1) M[r][j] *= s;
-		REP(i,N) if (i != r) REP(j,N+1) M[i][j] -= M[i][c] * M[r][j];
+		for(int i = r+1; i < R; i++) if(abs(m[i][c]) > abs(m[j][c])) j = i;
+		if(abs(m[j][c]) < EPS) continue;
+		swap(m[j], m[r]);
+		auto s = 1.0 / m[r][c];
+		REP(j,C) m[r][j] *= s;
+		REP(i,R) if (i != r) REP(j,C) m[i][j] -= m[i][c] * m[r][j];
 		++r;
 	}
 }
 
-	V ans; // the back substitution phase
-	for(auto j = N-1; j >= 0; --j){
-		bool allzero=true; long double t=0;
-		for(auto k = j+1; k < N; ++k){
-			t += M[j][k] * ans[k]; if(abs(M[j][k])>EPS) allzero=false;
-		}
-		if(abs(M[j][j])<EPS){
-			if(allzero && abs(M[j][N]) > EPS) mult = false, inconst = true;
-			else mult = true;
-		} else ans[j] = (M[j][N] - t) / M[j][j]; // the answer is here
+bool error, inconst;	// error => multiple or inconsistent
+template<int R, int C>	// Mx = a; M:R*R, v:R*C => x:R*C
+M<R,C> solve(const M<R,R> &m, const M<R,C> &a){
+	M<R,R+C> q;
+	REP(r,R){
+		REP(c,R) q[r][c] = m[r][c];
+		REP(c,R) q[r][R+c] = a[r][c];
 	}
-	return ans;
+	ReducedRowEchelonForm(q);
+	M<R,C> sol; error = false, inconst = false; 
+	REP(c,C) for(auto j = R-1; j >= 0; --j){
+		T t=0; bool allzero=true;
+		for(auto k = j+1; k < R; ++k)
+			t += q[j][k]*sol[k][c], allzero &= abs(q[j][k]) < EPS;
+		if(abs(q[j][j]) < EPS)
+			error = true, inconst |= allzero && abs(q[j][R+c]) > EPS;
+		else sol[j][c] = (q[j][R+c] - t) / q[j][j];
+	}
+	return sol;
 }
