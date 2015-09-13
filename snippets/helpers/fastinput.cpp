6a3cceb0b686b086155cd1817870cd5f7ba35a09
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2015-09-13 03:46:54 +0200

    add lazy and dynamic+lazy segment trees

--- snippets/helpers/fastinput.cpp
+++ snippets/helpers/fastinput.cpp
@@ -9,3 +9,17 @@ int r() {
 			default: n *= 10; n += c - '0'; break;
 		}
 }
+
+void scan(ll &x){	// doesn't handle negative numbers
+	char c;
+	while((x=getchar())<'0');
+	for(x-='0'; '0'<=(c=getchar()); x=10*x+c-'0');
+}
+void print(ll x){
+	char buf[20], *p=buf;
+	if(!x) putchar('0');
+	else{
+		while(x) *p++='0'+x%10, x/=10;
+		do putchar(*--p); while(p!=buf);
+	}
+}
