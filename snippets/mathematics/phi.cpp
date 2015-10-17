3f305c0efc817e9d42211c8558a48de9dec7fbf7
Timon Knigge <timonknigge@live.nl>
2015-10-17 22:50:33 +0200

    Adds O(n lg n) Euler Phi function.

---
+++ snippets/mathematics/phi.cpp
@@ -0,0 +1,7 @@
+#include "../header.h"
+void calculate_phi(int N, vector<int> &phi) {
+	phi.assgin(N + 1, 0);
+	iota(phi.begin(), phi.end(), 0); // numeric
+	for (int i = 2; i <= N; ++i) if (phi[i] == i)
+		for (int j = i; j <= N; j += i) phi[j] -= phi[j] / i;
+}
