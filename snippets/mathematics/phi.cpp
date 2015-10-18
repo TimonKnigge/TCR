397feb9a8aa3db32d1ab7f5095591529b36d92e6
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2015-10-18 02:04:35 +0200

    Fix typo

--- snippets/mathematics/phi.cpp
+++ snippets/mathematics/phi.cpp
@@ -1,6 +1,6 @@
 #include "../header.h"
 void calculate_phi(int N, vector<int> &phi) {
-	phi.assgin(N + 1, 0);
+	phi.assign(N + 1, 0);
 	iota(phi.begin(), phi.end(), 0); // numeric
 	for (int i = 2; i <= N; ++i) if (phi[i] == i)
 		for (int j = i; j <= N; j += i) phi[j] -= phi[j] / i;
