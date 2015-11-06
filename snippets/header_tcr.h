cbd52cbaaf4a4de435e62bea9abf83f25e20784a
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2015-11-06 13:03:25 +0100

    clean headers

--- snippets/header_tcr.h
+++ snippets/header_tcr.h
@@ -3,18 +3,10 @@ iostream string sstream vector list set map unordered_map queue stack bitset
 tuple cstdio numeric iterator algorithm cmath chrono cassert unordered_set
 using namespace std;	// :s/ /\r/g  :s/\w*/#include <\0>/g
 #define REP(i,n)	for(auto i = decltype(n)(0); i<(n); i++)
-#define F(v)		begin(v), end(v)
-constexpr bool LOG =
-#ifdef _LOG						// -D_LOG compiler option
-true;
-#define _GLIBCXX_DEBUG			// for bounds checking etc
-#else
-false;
-#endif
-using ll = long long; using ii = pair<int,int>; using vi = vector<int>;
-using vb = vector<bool>; using vvi = vector<vi>;
+using ll = long long; using vi = vector<int>;
 constexpr int  INF = 1e9+1; // < 1e9 - -1e9
 constexpr ll LLINF = 1e18+1;
+bool LOG = false;
 void Log() { if(LOG) cerr << "\n"; }
 template<class T, class... S> void Log(T t, S... s){
 	if(LOG) cerr << t << "\t", Log(s...);
