b58ac747c031b6c1eb0fa3e0b78f8f8b4dc4d89f
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2015-11-07 20:31:52 +0100

    change builtin to template

--- snippets/datastructures/builtin.cpp
+++ snippets/datastructures/builtin.cpp
@@ -1,12 +1,14 @@
 #include "../header.h"
 // Minimum Heap
 #include <queue>
+template<class T>
 using min_queue = priority_queue<T, vector<T>, greater<T>>;
 
 // Order Statistics Tree
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
+template<class TIn, class TOut>
 using order_tree = tree<
 	TIn, TOut, less<TIn>, // key, value types. TOut can be null_type
 	rb_tree_tag, tree_order_statistics_node_update>;
