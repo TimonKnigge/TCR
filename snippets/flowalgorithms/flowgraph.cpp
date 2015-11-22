a0c3ff7c20de7b0b0263e7f2d79457b12d1402fa
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2015-11-22 18:31:40 +0100

    Template for mincostflow

--- snippets/flowalgorithms/flowgraph.cpp
+++ snippets/flowalgorithms/flowgraph.cpp
@@ -1,16 +1,17 @@
 #include "../header.h"
+using F = ll; using W = ll; // types for flow and weight/cost
 struct S{
 	int v;			// neighbour
 	const int r;	// index of the reverse edge
-	ll f;			// current flow
-	const ll cap;	// capacity
-	const ll cost;	// unit cost
-	S(int v, int reverse_index, ll capacity, ll cost = 0) :
+	F f;			// current flow
+	const F cap;	// capacity
+	const W cost;	// unit cost
+	S(int v, int reverse_index, F capacity, W cost = 0) :
 		v(v), r(reverse_index), f(0), cap(capacity), cost(cost) {}
 };
 struct FlowGraph : vector<vector<S>> {
 	FlowGraph(size_t n) : vector<vector<S>>(n) {}
-	void add_edge(int u, int v, ll capacity, ll cost = 0){
+	void add_edge(int u, int v, F capacity, W cost = 0){
 		operator[](u).emplace_back(v, operator[](v).size(), capacity, cost);
 		operator[](v).emplace_back(u, operator[](u).size()-1, 0, -cost);
 	}
--- snippets/flowalgorithms/mincostflow.cpp
+++ snippets/flowalgorithms/mincostflow.cpp
@@ -1,35 +1,36 @@
 #include "../header.h"
 #include "flowgraph.cpp"
-struct Q{ int u; ll c,w;}; // target, maxflow and total weight (cost)
+using F = ll; using W = ll; W WINF = LLINF; F FINF = LLINF;
+struct Q{ int u; F c; W w;}; // target, maxflow and total cost/weight
 bool operator>(const Q &l, const Q &r){return l.w > r.w;}
 struct Edmonds_Karp_Dijkstra{
-	FlowGraph &g; int V,s,t; vector<ll> pot;
+	FlowGraph &g; int V,s,t; vector<W> pot;
 	Edmonds_Karp_Dijkstra(FlowGraph &g, int s, int t) :
 		g(g), V(g.size()), s(s), t(t), pot(V) {}
-	pair<ll,ll> run() { // return pair<f, cost>
-		ll maxflow = 0, cost = 0;
-		fill(F(pot), LLINF); pot[s]=0; // Bellman-Ford for potentials
+	pair<F,W> run() { // return pair<f, cost>
+		F maxflow = 0; W cost = 0;			// Bellmann-Ford for potentials
+		fill(pot.begin(),pot.end(),WINF); pot[s]=0;
 		REP(i,V-1) {
 			bool relax = false;
-			REP(u,V) if(pot[u] != LLINF) for(auto &e : g[u])
+			REP(u,V) if(pot[u] != WINF) for(auto &e : g[u])
 				if(e.cap>e.f)
 					if(pot[u] + e.cost < pot[e.v])
 						pot[e.v] = pot[u] + e.cost, relax=true;
 			if(!relax) break;
 		}
-		REP(u,V) if(pot[u] == LLINF) pot[u] = 0;
+		REP(u,V) if(pot[u] == WINF) pot[u] = 0;
 		while(true){
 			priority_queue<Q,vector<Q>,greater<Q>> q;
 			vector<vector<S>::iterator> p(V,g.front().end());
-			vector<ll> dist(V, LLINF); ll f, tf = -1;
-			q.push({s, LLINF, 0}); dist[s]=0;
+			vector<W> dist(V, WINF); F f, tf = -1;
+			q.push({s, FINF, 0}); dist[s]=0;
 			while(!q.empty()){
-				auto u = q.top().u; ll w = q.top().w;
+				int u = q.top().u; W w = q.top().w;
 				f = q.top().c; q.pop();
 				if(w!=dist[u]) continue; if(u==t && tf < 0) tf = f;
 				for(auto it = g[u].begin(); it!=g[u].end(); it++){
-					const auto &e = *it;
-					ll d =  w + e.cost + pot[u] - pot[e.v];
+					auto &e = *it;
+					W d =  w + e.cost + pot[u] - pot[e.v];
 					if(e.cap>e.f && d < dist[e.v]){
 						q.push({e.v, min(f, e.cap-e.f),dist[e.v] = d});
 						p[e.v]=it;
@@ -38,11 +39,11 @@ struct Edmonds_Karp_Dijkstra{
 			if(it == g.front().end()) return {maxflow,cost};
 			maxflow += f = tf;
 			while(it != g.front().end()){
-				auto & r = g[it->v][it->r];
+				auto &r = g[it->v][it->r];
 				cost += f * it->cost; it->f+=f;
 				r.f -= f; it = p[r.v];
 			}
-			REP(u,V) if(dist[u]!=LLINF) pot[u] += dist[u];
+			REP(u,V) if(dist[u]!=WINF) pot[u] += dist[u];
 		}
 	}
 };
