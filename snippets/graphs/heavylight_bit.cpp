815d1e12add7eb5f5f416d223604d2024511e4c5
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2015-12-23 02:37:23 +0100

    New HLD+BIT/segtree, using only 1 instance of BIT

--- snippets/graphs/heavylight_bit.cpp
+++ snippets/graphs/heavylight_bit.cpp
@@ -2,45 +2,32 @@
 #include "../datastructures/fenwick.cpp"
 
 struct HLD {
-	vi size;		// path-root; size of subtrees; h child
-	vi ts;		// dfs in and out times, toposort
-
-	int V; vvi &graph; // graph can be graph or childs only
-	vi p, r, d, h; // parents, path-root; h child, depth
-
-	vector<FenwickTree<ll>> fts;
-	// BIT stuff
-	vi pi;	// index in path
-	vi path;// number of the path
-
-	HLD(vvi &graph, int root = 0) : V(graph.size()), graph(graph),
-	p(V,-1), r(V,-1), d(V,0), h(V,-1), pi(V,-1), path(V,-1) { dfs(root);
-		for(int i=0; i<V; ++i) if (p[i]==-1 || h[p[i]]!=i){ // new path
-			int k = 1;
-			for (int j=i; j!=-1; j=h[j],++k)
-				r[j] = i, path[j]=fts.size(), pi[j]=k;
-			fts.push_back(FenwickTree<ll>(k));
-		}
+	int V; vvi &graph; FenwickTree<ll> ft; //graph may contain childs only
+	vi p, r, d, h, t; // parents, path-root; heavy, tree index
+	HLD(vvi &graph, int root = 0) : V(graph.size()), graph(graph), ft(V),
+	p(V,-1), r(V,-1), d(V,0), h(V,-1), t(V,-1){
+		dfs(root); int k=0;
+		for(int i=0; i<V; ++i) if (p[i]==-1 || h[p[i]]!=i)
+			for (int j=i; j!=-1; j=h[j]) r[j] = i, t[j]=++k;
 	}
 	int dfs(int u){
-		ii best; int s=1, ss;	// best, size (of subtree)
+		ii best={-1,-1}; int s=1, ss;	// best, size (of subtree)
 		for(auto &v : graph[u]) if(u!=p[v])
 			d[v]=d[u]+1, p[v]=u, s += ss=dfs(v), best = max(best,{ss,v});
 		h[u] = best.second; return s;
 	}
-	void update(int u, ll v){
-		fts[path[u]].update(pi[u],v);
-	}
-	ll query(int u){
-		if(u<0) return 0;
-		return fts[path[u]].query(pi[u]) + query(p[r[u]]);
-	}
-	ll query(int u, int v){
-		auto l = lca(u,v);
-		return query(u) + query(v) - query(l) - query(p[l]);
-	}
 	int lca(int u, int v){
 		for(; r[u]!=r[v]; v=p[r[v]]) if(d[r[u]] > d[r[v]]) swap(u,v);
 		return d[u] < d[v] ? u : v;
+	}
+	void update(int u, ll v){ ft.update(t[u],v); }
+	ll query(int u, int v){
+		ll ans=0;
+		for(; r[u]!=r[v]; v=p[r[v]]){
+			if(d[r[u]] > d[r[v]]) swap(u,v);
+			ans += ft.query(t[r[v]], t[v]);
+		}
+		if(d[u] > d[v]) swap(u,v);
+		return ans + ft.query(t[u],t[v]);
 	}
 };
--- snippets/graphs/heavylight_segtree.cpp
+++ snippets/graphs/heavylight_segtree.cpp
@@ -1,64 +1,35 @@
 #include "../header.h"
 #include "../datastructures/segmenttree.cpp"
 template <class T, T(*op)(T, T), T ident>
-struct HLD {
-	int V,t; vi &p; vvi &childs;	// Size; dfs-time; input parent/childs
-	vi pr, sz, heavy;				// path-root; subtree size; heavy child
-	vi t_in, t_out, ts;				// dfs in and out times, toposort
-	vi path, pi;		// only for segtree;  path number, index in path
-	vector<SegmentTree<T,op,ident>> sts;
-	HLD(vvi &childs, vi &p, vector<T> &init, int root = 0) :
-		V(p.size()), t(0), p(p), childs(childs), pr(V,-1), sz(V,-1),
-		heavy(V,-1), t_in(V,-1), t_out(V,-1), path(V,-1), pi(V,-1) {
-			stack<int> s; s.push(root);		// toposort in ts (top down)
-			while(!s.empty()){
-				int u = s.top(); s.pop(); t_in[u] = ++t; ts.push_back(u);
-				for(auto v : childs[u]) s.push(v);
-			}			// use sizes of subtrees to calculate heavy edges
-			for(auto it = ts.rbegin(); it!= ts.rend(); ++it){
-				int u = *it; sz[u] = 1; ii m{-1,-1};
-				auto &cs = childs[u];
-				for(auto v : cs) sz[u] += sz[v], m = max(m,{sz[v],v});
-				heavy[u] = m.second;
-				t_out[u] = cs.size()>0 ? t_out[cs[0]] : t_in[u];
-			}
-			segtrees(init);
-		}
-	void segtrees(vector<T> &init){
-		vector<vector<T>> pinit;					// one vector per path
-		for(auto u : ts){
-			if(u != 0 && u == heavy[p[u]]){
-				pr[u] = pr[p[u]]; pi[u] = pi[p[u]]+1;
-				path[u] = path[p[u]]; pinit[path[u]].push_back(init[u]);
-			} else {
-				pr[u] = u; pi[u]=0;
-				path[u] = pinit.size(); pinit.push_back({init[u]});
-			}
-		}
-		for(auto x : pinit){						// build segmenttree
-			int s= 32 - __builtin_clz(x.size()-1);
-			sts.push_back(SegmentTree<T,op,ident>(s, x));
-		}
+struct HLD { //graph may contain childs only
+	int V; vvi &graph; SegmentTree<T,op,ident> st;
+	vi p, r, d, h, t; // parents, path-root, depth heavy, tree index
+	HLD(vvi &graph, vector<T> &init, int root = 0) :
+		V(graph.size()), graph(graph), st({}),
+	p(V,-1), r(V,-1), d(V,0), h(V,-1), t(V,-1){
+		dfs(root); int k=0; vector<T> v(V);
+		for(int i=0; i<V; ++i) if (p[i]==-1 || h[p[i]]!=i)
+			for (int j=i; j!=-1; j=h[j]) r[j] = i, v[k]=init[j], t[j]=k++;
+		st={v};
 	}
-	bool is_parent(int p, int u){	// test whether p is a parent of u
-		return t_in[p] <= t_in[u] && t_out[u] <= t_out[p];
+	int dfs(int u){
+		ii best={-1,-1}; int s=1, ss;	// best, size (of subtree)
+		for(auto &v : graph[u]) if(u!=p[v])
+			d[v]=d[u]+1, p[v]=u, s += ss=dfs(v), best = max(best,{ss,v});
+		h[u] = best.second; return s;
 	}
 	int lca(int u, int v){
-		while(!is_parent(pr[v],u)) v = p[pr[v]];
-		while(!is_parent(pr[u],v)) u = p[pr[u]];
-		return is_parent(u,v) ? u : v;
-	}
-	void update(int u, T v){
-		sts[path[u]].update(pi[u],v);
-	}
-	T query(int u, int l, bool inc){			// query up to lca
-		T a = ident;
-		while(path[u]!=path[l])
-			a = op(a, sts[path[u]].query(0, pi[u])), u = p[pr[u]];
-		return op(a, sts[path[u]].query(pi[l] + (inc ? 0 : 1), pi[u]));
+		for(; r[u]!=r[v]; v=p[r[v]]) if(d[r[u]] > d[r[v]]) swap(u,v);
+		return d[u] < d[v] ? u : v;
 	}
 	T query(int u, int v){
-		auto l = lca(u,v);			// include l the right number of times!
-		return op(query(u,l,true), query(v,l,false));	//2x false for edges
+		T a = ident;
+		for(; r[u]!=r[v]; v=p[r[v]]){
+			if(d[r[u]] > d[r[v]]) swap(u,v);
+			a = op(a,st.query(t[r[v]], t[v]));
+		}
+		if(d[u] > d[v]) swap(u,v);
+		return op(a,st.query(t[u],t[v])); // t[u]+1 if data is on edges
 	}
+	void update(int u, ll v){ st.update(t[u],v); }
 };
