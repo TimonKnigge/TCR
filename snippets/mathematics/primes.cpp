00ab01097745738bf5b4e23f1504a2931206227b
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2015-12-28 00:40:41 +0100

    fix duplicate divisors

--- snippets/mathematics/primes.cpp
+++ snippets/mathematics/primes.cpp
@@ -39,8 +39,8 @@ vector<ll> divisors(ll n){
 	auto fs = factor(n); vector<ll> ds{1};
 	ds.reserve(accumulate(fs.begin(),fs.end(),1,
 		[](ll a,auto f){return a*(f.exp+1);}));
-	for(auto f : fs) for(auto d : ds) for(ll i = 0; i <= f.exp; ++i)
-				ds.push_back(d), d*=f.prime;
+	for(auto f : fs) for(auto d : ds) for(ll i = 0; i < f.exp; ++i)
+				ds.push_back(d*=f.prime);
 	return ds;
 }
 
