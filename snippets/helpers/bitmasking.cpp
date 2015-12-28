2f7f1f8bfc97ea337d4fa6fe3e015526339fe039
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2015-12-28 15:05:04 +0100

    Add functions for mask loops

--- snippets/helpers/bitmasking.cpp
+++ snippets/helpers/bitmasking.cpp
@@ -5,50 +5,44 @@
 #define popcount(x) __builtin_popcount(x)
 #endif
 
-bool bit_set(int mask, int pos) {
-	return ((mask & (1 << pos)) != 0);
+template<typename F>	// f(ll mask) is called
+void iterate_k_subset(ll N, ll k, F f){
+	ll mask = (1 << k) - 1;
+	while (!(mask & 1 << N)) { f(mask);
+		ll lo = mask & ~(mask - 1);
+		ll lz = (mask + lo) & ~mask;
+		mask |= lz;
+		mask &= ~(lz - 1);
+		mask |= ((lz / lo) >> 1) - 1;
+	}
+}
+template<typename F>	// f(ll mask) is called
+void iterate_k_subset2(ll N, ll k, F f){
+	ll mask = (1 << k) - 1;
+	while (!(mask & 1 << N)) { f(mask);
+		ll t = mask | (mask - 1);
+		mask = (t + 1) | (((~t & -~t) - 1) >> (__builtin_ctz(mask) + 1));
+	}
 }
 
-// Iterate over all subsets of a set of size N
-for (int mask = 0; mask < (1 << N); ++mask) {
-	// Decode mask here
+// Iterate over all subsets of a set
+template<typename F>	// f(ll mask) is called
+void iterate_mask_subset(ll set, F f){
+	ll mask = set;
+	do  f(mask), mask = (mask - 1) & set;
+	while (mask != set);
 }
-
-// Iterate over all k-subsets of a set of size N
-int mask = (1 << k) - 1;
-while (!(mask & 1 << N)) {
-	// Decode mask here
-	int lo = mask & ~(mask - 1);
-	int lz = (mask + lo) & ~mask;
-	mask |= lz;
-	mask &= ~(lz - 1);
-	mask |= ((lz / lo) >> 1) - 1;
-}
-
-// Same as above
-int mask = (1 << k) - 1;
-while (!(mask & 1 << N)) { // Decode mask here
-	int t = msk2 | (msk2 - 1);
-	msk2 = (t + 1) | (((~t & -~t) - 1) >> (__builtin_ctz(msk2) + 1));
-}
-
-// Iterate over all subsets of a subset
-int mask = givenMask;
-do {
-	// Decode mask here
-	mask = (mask - 1) & givenMask;
-} while (mask != givenMask);
 
 // The two functions below are used in the FFT:
-inline int next_power_of_2(int x) {
+ll next_power_of_2(ll x) {
 	x = (x - 1) | ((x - 1) >> 1);
 	x |= x >> 2; x |= x >> 4;
 	x |= x >> 8; x |= x >> 16;
 	return x + 1;
 }
 
-inline int brinc(int x, int k) {
-	int I = k - 1, s = 1 << I;
+ll brinc(ll x, ll k) {
+	ll I = k - 1, s = 1 << I;
 	x ^= s;
 	if ((x & s) != s) {
 		I--; s >>= 1;
