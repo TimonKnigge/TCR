364c6661338146f4010bffd0b8b1993311d42066
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2016-01-09 01:19:53 +0100

    fix toposort

--- snippets/graphs/toposort.cpp
+++ snippets/graphs/toposort.cpp
@@ -5,14 +5,14 @@ struct Toposort {
 	vi sorted, visited;
 
 	Toposort(vector<vi> &edges) :
-		edges(edges), V(edges.size()), s_ix(0),
+		edges(edges), V(edges.size()), s_ix(V),
 		sorted(V,-1), visited(V,false) {}
 
 	void visit(int u) {
 		visited[u] = true;
 		for (int v : edges[u])
 			if (!visited[v]) visit(v);
-		sorted[s_ix--] = u;
+		sorted[--s_ix] = u;
 	}
 	void topo_sort() {
 		REP(i,V) if (!visited[i]) visit(i);
