ca55beb3c2672c38d6b7129c19e122d34b264c60
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2016-02-08 11:00:33 +0100

    fix bugs

--- snippets/datastructures/segmenttree_dynamic.cpp
+++ snippets/datastructures/segmenttree_dynamic.cpp
@@ -5,30 +5,28 @@ T op(T a, T b){ return a+b; }
 void join(U &a, U b){ a+=b; }
 void apply(T &t, U u, int x){ t+=x*u; }
 T part(T t, int r, int p){ return t/r*p; }
-struct Node { int l, r, lc, rc; T t; U u;
-	Node(int l, int r, T t=t_id):l(l),r(r),lc(-1),rc(-1),t(t),u(u_id){}
-};
-void push(Node &n, U u){ apply(n.t, u, n.r-n.l); join(n.u,u); }
 struct DynamicSegmentTree {
+	struct Node { int l, r, lc, rc; T t; U u;
+		Node(int l, int r):l(l),r(r),lc(-1),rc(-1),t(t_id),u(u_id){}
+	};
 	vector<Node> tree;
 	DynamicSegmentTree(int N) { tree.push_back({0,N}); }
+	void push(Node &n, U u){ apply(n.t, u, n.r-n.l); join(n.u,u); }
+	void push(Node &n){push(tree[n.lc],n.u);push(tree[n.rc],n.u);n.u=u_id;}
 	T query(int l, int r, int i = 0) { auto &n = tree[i];
-		if(l >= n.r || r <= n.l) return t_id;
+		if(r <= n.l || n.r <= l) return t_id;
 		if(l <= n.l && n.r <= r) return n.t;
 		if(n.lc < 0) return part(n.t, n.r-n.l, min(n.r,r)-max(n.l,l));
-		//push(tree[n.lc], n.u); push(tree[n.rc], n.u); n.u = u_id;
-		//return op(query(l, r, n.lc), query(l, r, n.rc));
-		return op(n.u*(min(n.r,r)-max(n.l,l)),op(query(l, r, n.lc), query(l, r, n.rc)));
+		return push(n), op(query(l,r,n.lc),query(l,r,n.rc));
 	}
 	void update(int l, int r, U u, int i = 0) { auto &n = tree[i];
 		if(r <= n.l || n.r <= l) return;
 		if(l <= n.l && n.r <= r) return push(n,u);
-		if(n.lc < 0 || n.rc < 0) { int m = (n.l + n.r) / 2;
-			n.lc = tree.size(); n.rc = n.lc+1;
-			tree.push_back({tree[i].l, m});
-			tree.push_back({m, tree[i].r});
+		if(n.lc < 0) { int m = (n.l + n.r) / 2;
+			n.lc = tree.size();				n.rc = n.lc+1;
+			tree.push_back({tree[i].l, m});	tree.push_back({m, tree[i].r});
 		}
-		apply(tree[i].t, u, min(tree[i].r,r)-max(tree[i].l,l));
-		update(l,r,u,tree[i].lc); update(l,r,u,tree[i].rc);
+		push(tree[i]); update(l,r,u,tree[i].lc); update(l,r,u,tree[i].rc);
+		tree[i].t = op(tree[tree[i].lc].t, tree[tree[i].rc].t);
 	}
 };
--- snippets/datastructures/segmenttree_lazy.cpp
+++ snippets/datastructures/segmenttree_lazy.cpp
@@ -4,34 +4,34 @@ T t_id; U u_id;
 T op(T a, T b){ return a+b; }
 void join(U &a, U b){ a+=b; }
 void apply(T &t, U u, int x){ t+=x*u; }
-struct Node { int l, r, lc, rc; T t; U u;
-	Node(int l, int r, T t=t_id):l(l),r(r),lc(-1),rc(-1),t(t),u(u_id){}
-};
-void push(Node &n, U u){ apply(n.t, u, n.r-n.l); join(n.u,u); }
-T convert(const I &i){ return i; }		// or make a T(I) constructor
+T convert(const I &i){ return i; }
 struct LazySegmentTree {
+	struct Node { int l, r, lc, rc; T t; U u;
+		Node(int l, int r, T t=t_id):l(l),r(r),lc(-1),rc(-1),t(t),u(u_id){}
+	};
 	int N; vector<Node> tree; vector<I> &init;
 	LazySegmentTree(vector<I> &init) : N(init.size()), init(init){
 		tree.reserve(2*N-1); tree.push_back({0,N}); build(0, 0, N);
 	}
 	void build(int i, int l, int r) { auto &n = tree[i];
 		if (r > l+1) { int m = (l+r)/2;
-			n.lc = tree.size();		n.rc = tree.size()+1;
-			tree.push_back({l,m});	tree.push_back({m,r});
-			build(n.lc,l,m);	build(n.rc,m,r);
+			n.lc = tree.size();			n.rc = n.lc+1;
+			tree.push_back({l,m});		tree.push_back({m,r});
+			build(n.lc,l,m);			build(n.rc,m,r);
 			n.t = op(tree[n.lc].t, tree[n.rc].t);
 		} else n.t = convert(init[l]);
 	}
+	void push(Node &n, U u){ apply(n.t, u, n.r-n.l); join(n.u,u); }
+	void push(Node &n){push(tree[n.lc],n.u);push(tree[n.rc],n.u);n.u=u_id;}
 	T query(int l, int r, int i = 0) { auto &n = tree[i];
 		if(r <= n.l || n.r <= l) return t_id;
 		if(l <= n.l && n.r <= r) return n.t;
-		push(tree[n.lc], n.u); push(tree[n.rc], n.u); n.u = u_id;
-		return op(query(l,r,n.lc),query(l,r,n.rc));
+		return push(n), op(query(l,r,n.lc),query(l,r,n.rc));
 	}
 	void update(int l, int r, U u, int i = 0) { auto &n = tree[i];
-		if(l > n.r || r < n.l) return;
-		if(l <= n.l && n.r <= r){ push(n,u); return; }
-		apply(n.t, u, min(n.r,r)-max(n.l,l));
-		update(l,r,u,n.lc); update(l,r,u,n.rc);
+		if(r <= n.l || n.r <= l) return;
+		if(l <= n.l && n.r <= r) return push(n,u);
+		push(n); update(l,r,u,n.lc); update(l,r,u,n.rc);
+		n.t = op(tree[n.lc].t, tree[n.rc].t);
 	}
 };
