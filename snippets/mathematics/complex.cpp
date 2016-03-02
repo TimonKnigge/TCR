9a9b37e3db3a4ad38fa333f24236f7637a914248
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2016-03-03 00:22:06 +0100

    add pi

--- snippets/mathematics/complex.cpp
+++ snippets/mathematics/complex.cpp
@@ -1,4 +1,5 @@
 #include "../header.h"
+constexpr ld pi = 3.1415926535897932384626433;
 struct Complex { using T = Complex; ld u,v;
 	Complex(ld u=0, ld v=0) : u{u}, v{v} {}
 	T operator+(T r) const { return {u+r.u, v+r.v}; }
@@ -9,5 +10,5 @@ struct Complex { using T = Complex; ld u,v;
 		return {(u*r.u + v*r.v)/norm, (v*r.u - u*r.v)/norm};
 	}
 	T inv(){ return T{1,0}/ *this; }
-	static T root(ll k){ return {cos(2*M_PIl/k), sin(2*M_PIl/k)}; }
+	static T root(ll k){ return {cos(2*pi/k), sin(2*pi/k)}; }
 };
