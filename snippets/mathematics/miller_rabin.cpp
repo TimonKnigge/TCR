b98b24fcf26106c6c1a19de1262d0ad425b8ed32
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2016-03-20 15:29:30 +0100

    reorganize numbertheory; add vector<ll> phis(n)

---
+++ snippets/mathematics/miller_rabin.cpp
@@ -0,0 +1,24 @@
+#include "../header.h"
+#include "numbertheory.cpp"
+vector<ll> test_primes = {2,3,5,7,11,13,17,19,23};	// <= 3.8e18
+vector<ll> test_primes2= {2,13,23,1662803};			// <= 1.1e12
+//using ll = __int128	// uncomment for n>=1e9, or use mulmod instead
+bool miller_rabin(const ll n){	// true when prime
+	if(n<2) return false;		
+	if(n%2==0) return n==2;
+	ll s = 0, d = n-1; // n-1 = 2^s * d
+	while(~d&1) s++, d/=2;
+	for(auto a : test_primes){
+		if(a > n-2) break;
+		ll x = powmod(a,d,n);
+		if(x == 1 || x == n-1) continue;
+		REP(i,s-1){
+			x = x*x%n;
+			if(x==1) return false;
+			if(x==n-1) goto next_it;
+		}
+		return false;
+next_it:;
+	}
+	return true;
+}
--- snippets/mathematics/primes.cpp
+++ snippets/mathematics/primes.cpp
@@ -1,8 +1,6 @@
 #include "../header.h"
 #include "numbertheory.cpp"
-ll SIZE;
-vector<bool> bs;
-vector<ll> primes;
+ll SIZE; vector<bool> bs; vector<ll> primes, mf;// mf[i]==i when prime
 
 void sieve(ll size = 1e6) { // call at start in main!
 	SIZE = size; bs.assign(SIZE+1,1);
@@ -12,39 +10,23 @@ void sieve(ll size = 1e6) { // call at start in main!
 		primes.push_back(i);
 	}
 }
-
 bool is_prime(ll n) { // for N <= SIZE^2
 	if (n <= SIZE) return bs[n];
 	for(const auto &prime : primes)
 	   	if (n % prime == 0) return false;
 	return true;
 }
-
-struct Factor{ll prime; ll exp;}; using Fs = vector<Factor>;
-Fs factor(ll n) {
-	Fs factors;
-	for(const auto &prime : primes){
-		if(n==1 || prime*prime > n) break;
-		ll exp=0;
-		while(n % prime == 0)
-		   	n/=prime, exp++;
-		if(exp>0)
-			factors.push_back({prime,exp});
+struct Factor{ll p; ll exp;}; using FS = vector<Factor>;
+FS factor(ll n) { FS fs;
+	for(const auto &p: primes){ ll exp=0;
+		if(n==1 || p*p > n) break;
+		while(n % p == 0) n/=p, exp++;
+		if(exp>0) fs.push_back({p,exp});
 	}
-	if (n != 1) factors.push_back({n,1});
-	return factors;
+	if (n != 1) fs.push_back({n,1});
+	return fs;
 }
 
-vector<ll> divisors(const Fs &fs){
-	vector<ll> ds{1};
-	ds.reserve(accumulate(fs.begin(),fs.end(),1,
-		[](ll a,auto f){return a*(f.exp+1);}));
-	for(auto f : fs) for(auto d : ds) for(ll i = 0; i < f.exp; ++i)
-				ds.push_back(d*=f.prime);
-	return ds;
-}
-
-vector<ll> mf;				// mf[i]==i when prime
 void sieve2(ll size=1e6) {	// call at start in main!
 	SIZE = size; mf.assign(SIZE+1,-1);
 	mf[0] = mf[1] = 1;
@@ -56,48 +38,28 @@ void sieve2(ll size=1e6) {	// call at start in main!
 	}
 }
 bool is_prime2(ll n) { assert(n<=SIZE); return mf[n]==n; }
-Fs factor2(ll n){
-	Fs factors;
-	while(n>1){
-		if(!factors.empty() && factors.back().prime == mf[n])
-			factors.back().exp++;
-		else factors.push_back({mf[n],1});
-		n/=mf[n];
-	}
-	return factors;
+FS factor2(ll n){ FS fs;
+	for(; n>1; n/=mf[n])
+		if(!fs.empty() && fs.back().p== mf[n]) fs.back().exp++;
+		else fs.push_back({mf[n],1});
+	return fs;
 }
 
-ll num_div(  const Fs &fs) { ll d = 1;
+vector<ll> divisors(const FS &fs){ vector<ll> ds{1};
+	ll s=1; for(auto &f:fs) s*=f.exp+1; ds.reserve(s);
+	for(auto f : fs) for(auto d : ds) for(ll i=0; i<f.exp; ++i)
+		ds.push_back(d*=f.p);
+	return ds;
+}
+vector<ll> phis(ll n){ vi p(n+1,0); iota(all(p), 0);
+	for(ll i=1; i<=n; ++i) for(ll j=2*i; j<=n; j+=i) p[j]-=p[i]; return p;}
+ll num_div(  const FS &fs) { ll d = 1;
 	for(auto &f : fs) d *= f.exp+1; return d; }
-ll sum_div(  const Fs &fs) { ll s = 1;
-	for(auto &f : fs) s *= (pow(f.prime,f.exp+1)-1)/(f.prime-1); return s; }
-ll phi(ll n, Fs fs={}    ) { ll p = n; if(fs.empty()) fs=factor(n);
-	for(auto &f : fs) p -= p/f.prime; return p; }
-ll ord(ll n, ll m){ ll o = phi(m);		// n^ord(n,m)=1 mod m
-	for(auto f : factor(o))
-		while(f.exp-- && powmod(n,o/f.prime,m)==1) o/=f.prime;
-	return o;
-}
+ll sum_div(  const FS &fs) { ll s = 1;
+	for(auto &f : fs) s *= (pow(f.p,f.exp+1)-1)/(f.p-1); return s; }
+ll phi(ll n, const FS &fs) { ll p = n;
+	for(auto &f : fs) p -= p/f.p; return p; }
+ll ord(ll n, ll m, const FS &fs){ ll o = phi(m,fs);	// n^ord(n,m)=1 mod m
+	for(auto f : factor(o)) while(f.exp-- && powmod(n,o/f.p,m)==1) o/=f.p;
+	return o; }
 
-vector<ll> test_primes = {2,3,5,7,11,13,17,19,23};	// <= 3.8e18
-vector<ll> test_primes2= {2,13,23,1662803};			// <= 1.1e12
-//using ll = __int128
-bool miller_rabin(const ll n){	// true when prime
-	if(n<2) return false;		
-	if(n%2==0) return n==2;
-	ll s = 0, d = n-1; // n-1 = 2^s * d
-	while(~d&1) s++, d/=2;
-	for(auto a : test_primes){
-		if(a > n-2) break;
-		ll x = powmod(a,d,n);
-		if(x == 1 || x == n-1) continue;
-		REP(i,s-1){
-			x = x*x%n;
-			if(x==1) return false;
-			if(x==n-1) goto next_it;
-		}
-		return false;
-next_it:;
-	}
-	return true;
-}
