1cbb8edb95888d726db796e758533c4407313e33
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2016-04-13 17:08:32 +0200

    shrink vimrc; update cpp header

--- snippets/header_tcr.h
+++ snippets/header_tcr.h
@@ -1,17 +1,13 @@
 //lstlistings skips the first [this] line
 //#define _GLIBCXX_DEBUG
-//#include <bits/stdc++.h>
-iostream string sstream vector list set map unordered_map queue stack bitset
-tuple cstdio numeric iterator algorithm cmath chrono cassert unordered_set
+#include <bits/stdc++.h>
+//iostream string sstream vector list set map queue stack bitset
+//tuple cstdio numeric iterator algorithm cmath chrono cassert
 using namespace std;	// :s/ /\r/g  :s/\w*/#include <\0>/g
 #define REP(i,n)	for(auto i = decltype(n)(0); i<(n); i++)
-using ll = long long; using vi = vector<int>;
-const int  INF = 1e9+1; const ll LLINF = 1e18+1; const bool LOG = false;
-void Log() { if(LOG) cerr << "\n"; }
+#define all(x)		x.begin(), x.end()
+using ll = long long; using ld = long double; using vi = vector<ll>;
+const bool LOG = false; void Log() { if(LOG) cerr << "\n"; }
 template<class T, class... S> void Log(T t, S... s){
-	if(LOG) cerr << t << "\t", Log(s...);
-}
-int main(){
-	ios::sync_with_stdio(false); cin.tie(nullptr);
-	return 0;
-}
+	if(LOG) cerr << t << "\t", Log(s...); }
+int main(){ ios::sync_with_stdio(false); cin.tie(nullptr); return 0; }
