06ca589b94696212484e1bb44e5f38b7cc7306a8
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2016-04-26 16:02:21 +0200

    Fix bug in TCR

--- snippets/flowalgorithms/dinic_ragnar.cpp
+++ snippets/flowalgorithms/dinic_ragnar.cpp
@@ -5,7 +5,7 @@ struct Dinic{
 	vi l; vector<vector<S>::iterator> its; // levels and iterators
 	Dinic(FlowGraph &edges, int s, int t) :
 		edges(edges), V(edges.size()), s(s), t(t), l(V,-1), its(V) {}
-	ll augment(int u, ll c) { // we reuse the same iterators
+	ll augment(int u, F c) { // we reuse the same iterators
 		if (u == t) return c;
 		for(auto &i = its[u]; i != edges[u].end(); i++){
 			auto &e = *i;
