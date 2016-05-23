5d67eab9c88afb81ba2aa7f13ceb44398539220d
Timon Knigge <TimonKnigge@users.noreply.github.com>
2016-05-23 19:28:09 +0200

    Modifies the segmenttree so it doesn't take an init-vector
    This usecase occurs more often.

--- snippets/datastructures/segmenttree.cpp
+++ snippets/datastructures/segmenttree.cpp
@@ -2,11 +2,7 @@
 template <class T, T(*op)(T, T), T ident>
 struct SegmentTree {
 	int n; vector<T> tree;
-	SegmentTree(vector<T> &init) : n(init.size()), tree(2 * n, ident) {
-		copy(init.begin(), init.end(), tree.begin() + n);
-		for (int j = n - 1; j > 0; --j)
-			tree[j] = op(tree[2*j], tree[2*j+1]);
-	}
+	SegmentTree(int _n) : n(_n), tree(2 * n, ident) { }
 	void update(int i, T val) {
 		for (tree[i+n] = val, i = (i+n)/2; i > 0; i /= 2)
 			tree[i] = op(tree[2*i], tree[2*i+1]);
