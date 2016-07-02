242f0f69a39abf17f83ba9be67dbd0a4349b5467
Timon Knigge <timonknigge@live.nl>
2016-07-02 16:09:21 +0200

    Adds O(1) resettable vector.

---
+++ snippets/datastructures/resettable_vector.cpp
@@ -0,0 +1,14 @@
+
+template <class T>
+struct resettable_vector {
+	vector<T> val, def;
+	vi time; int cur = 0;
+	
+	void init(int n, T d) {
+		val.assign(n, d); def.assign(n, d); time.assign(n, cur = 0);
+	}
+	
+	T get(int i) { return time[i] == cur ? val[i] : def[i]; }
+	void set(int i, T v) { val[i] = v; time[i] = cur; }
+	void reset() { ++cur; }
+};
