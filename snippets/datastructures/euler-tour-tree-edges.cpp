70550e02ec0122c765b3adf094913cba10804866
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2017-07-13 22:01:41 +0100

    update eulertourtree

--- snippets/datastructures/euler-tour-tree-edges.cpp
+++ snippets/datastructures/euler-tour-tree-edges.cpp
@@ -1,58 +1,55 @@
 #include "../header.h"
 #include "sequence.cpp"
-
 struct EulerTourForest {
-	struct T {
-		int u, v; // edge from u to v
+	struct edge {
+		int u, v;
 	};
-	using node = seq<T>;
+	using node = seq<edge>;
 	// An edge_handler is returned when creating a vertex.
 	// This has to passed back in to delete the edge
 	using ptr = std::unique_ptr<node>;
-	using edge_handler = array<ptr, 2>;
+	struct edge_handler : array<ptr, 2> {
+		edge_handler(edge_handler &&e) : array<ptr, 2>{move(e)} {
+			for(auto &p : e) p.reset();
+		}
+		edge_handler(ptr &&l, ptr &&r) : array<ptr, 2>{{move(l), move(r)}} {}
+		~edge_handler() {}
+	};
 	vector<node> nodes; // one node for each vertex
-
-	// Create a new vertex.
+	EulerTourForest(int n = 0) {
+		nodes.reserve(n);
+		while(n--) add_vertex();
+	}
 	int add_vertex() {
 		int v = nodes.size();
-		nodes.emplace_back(T{v, v});
+		nodes.emplace_back(edge{v, v});
 		return v;
 	}
-	// Find root of the subtree containing this vertex.
-	int root(int v) { return nodes[v].root()->min()->val.u; }
-	// make v the root of the tree
-	void make_root(node *x) {
-		// xxx v yyy -> v yyy xxx
-		if(x->p == nullptr) return;
-		auto[left, right] = split(x->root(), x); // left: xxx, right: v yyy
-		merge(right, left);
+	int root(int v) { return nodes[v].root()->max()->val.u; }
+	node *reroot(node *x) {
+		if(x == nullptr) return x;
+		auto[left, right] = split(x);
+		return merge(right, left);
 	}
-	void make_root(int v) { make_root(&nodes[v]); }
-
-	// Cut from parent (if exists).
+	void reroot(int v) { reroot(&nodes[v]); }
 	void cut(edge_handler &&e) {
 		if(e[0]->index() > e[1]->index()) swap(e[0], e[1]);
 		// (xxx e0 yyy e1 zzz) -> (xxx zzz, yyy)
 		auto xeyez = e[0]->root();
-		auto[x, eyez] = split(xeyez, e[0].get());
-		auto[ey, ez] = split(eyez, e[1].get());
-		auto[e0, y] = split(ey, e[0]->next()); // or e[0]->index()+1
-		auto[e1, z] = split(ez, e[1]->next()); // or e[1]->index()+1
+		auto[x, eyez] = split(e[0].get()); // xeyez
+		auto[ey, ez] = split(e[1].get());  // eyez
+		auto[e0, y] = split(ey, 1);        // ey
+		auto[e1, z] = split(ez, 1);        // ez
 		merge(x, z);
 	}
-
-	// Make v the parent of u. Reroots u when needed
-	edge_handler attach(int u, int v) {
-		// (xxx u yyy, aaa v bbb)
-		auto x = &nodes[v], y = &nodes[v];
-		make_root(x); // (u yyy xxx, aaa v bbb)
-		//  aaa v e0 u yyy xxx e1 bbb
-		edge_handler e{{ptr{new node{{v, u}}}, ptr{new node{{v, u}}}}};
-		auto[av, b] = split(y->root(), y->next()); // or v->index()+1
-		::merge(av, e[0].get(), x, e[1].get(), b);
+	edge_handler link(int u, int v) {
+		auto x = reroot(&nodes[u]), y = &nodes[v];
+		auto[a, vb] = split(y); // or v->index()+1
+		edge_handler e{ptr{new node{{v, u}}}, ptr{new node{{u, v}}}};
+		::merge(a, e[0].get(), x, e[1].get(), vb);
 		return e;
 	}
-	int size(int u) { return (nodes[u].root()->size + 2) / 3; }
+	int size(int u) { return (nodes[u].root()->size_ + 2) / 3; }
 	bool connected(int u, int v) {
 		return nodes[u].root() == nodes[v].root();
 	}
--- snippets/datastructures/sequence.cpp
+++ snippets/datastructures/sequence.cpp
@@ -68,22 +68,25 @@ pair<seq<T> *, seq<T> *> split(seq<T> *A, int index) {
 // return [0, A), [A, ..)
 template <class T>
 pair<seq<T> *, seq<T> *> split(seq<T> *A) {
-	if (A == nullptr) return {nullptr, nullptr};
-	seq<T> *B = A, *lr = A; A = A->l;
-	if (A == nullptr) {
-		while (lr->p != nullptr && lr->p->l == B) lr = B = lr->p;
-		if (lr->p != nullptr) {
-			lr = A = lr->p; lr->r = B->p = nullptr;
+	if(A == nullptr) return {nullptr, nullptr};
+	seq<T> *B = A, *lr = A;
+	A = A->l;
+	if(A == nullptr) {
+		while(lr->p != nullptr && lr->p->l == B) lr = B = lr->p;
+		if(lr->p != nullptr) {
+			lr = A = lr->p;
+			lr->r = B->p = nullptr;
 		}
-	} else	A->p = lr->l = nullptr;
-	while (lr->update()->p != nullptr) {
-			if (lr->p->l == lr) {
-				if (lr == A) swap(A->p, B->p), B->p->l = B;
-				lr = B = B->p;
-			} else {
-				if (lr == B) swap(A->p, B->p), A->p->r = A;
-				lr = A = A->p;
-			}
-	}			
+	} else
+		A->p = lr->l = nullptr;
+	while(lr->update()->p != nullptr) {
+		if(lr->p->l == lr) {
+			if(lr == A) swap(A->p, B->p), B->p->l = B;
+			lr = B = B->p;
+		} else {
+			if(lr == B) swap(A->p, B->p), A->p->r = A;
+			lr = A = A->p;
+		}
+	}
 	return {A, B};
 }
