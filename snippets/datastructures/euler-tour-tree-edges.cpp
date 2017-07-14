78bf2686b8f641d30f55d47add6f743179a3e7b0
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2017-07-14 02:21:36 +0100

    add merge function to sequence

--- snippets/datastructures/euler-tour-tree-edges.cpp
+++ snippets/datastructures/euler-tour-tree-edges.cpp
@@ -1,19 +1,21 @@
 #include "../header.h"
 #include "sequence.cpp"
+template <class T, void M(const T *, T *, const T *) = T::merge>
 struct EulerTourForest {
 	struct edge {
+		T val;
 		int u, v;
+		static void merge(const edge *l, edge *m, const edge *r) {
+			M(l ? &l->val : nullptr, m->val, r ? &r->val : nullptr);
+		}
 	};
 	using node = seq<edge>;
-	// An edge_handler is returned when creating a vertex.
-	// This has to passed back in to delete the edge
 	using ptr = std::unique_ptr<node>;
 	struct edge_handler : array<ptr, 2> {
 		edge_handler(edge_handler &&e) : array<ptr, 2>{move(e)} {
 			for(auto &p : e) p.reset();
 		}
 		edge_handler(ptr &&l, ptr &&r) : array<ptr, 2>{{move(l), move(r)}} {}
-		~edge_handler() {}
 	};
 	vector<node> nodes; // one node for each vertex
 	EulerTourForest(int n = 0) {
@@ -34,13 +36,11 @@ struct EulerTourForest {
 	void reroot(int v) { reroot(&nodes[v]); }
 	void cut(edge_handler &&e) {
 		if(e[0]->index() > e[1]->index()) swap(e[0], e[1]);
-		// (xxx e0 yyy e1 zzz) -> (xxx zzz, yyy)
-		auto xeyez = e[0]->root();
-		auto x = split(e[0].get()).first;       // xeyez
-		auto ey_ez = split(e[1].get());         // eyez
-		auto y = split(ey_ez.first, 1).second;  // ey
-		auto z = split(ey_ez.second, 1).second; // ez
-		merge(x, z);
+		auto x = split(e[0].get()).first;       // xeyez -> (x,eyez)
+		auto ey_ez = split(e[1].get());         // eyez  -> (ey,ez)
+		split(ey_ez.first, 1).second;           // ey    -> (e,y)
+		auto z = split(ey_ez.second, 1).second; // ez    -> (e,z)
+		merge(x, z); // the edge_handler will be destructed at the end.
 	}
 	edge_handler link(int u, int v) {
 		auto x = reroot(&nodes[u]), y = &nodes[v];
