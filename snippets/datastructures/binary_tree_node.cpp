cfedd4325fc809ea3f77551381cf16b896de8bbc
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2017-07-15 21:41:58 +0100

    add tested splay_tree and binary_search_tree

--- snippets/datastructures/binary_tree_node.cpp
+++ snippets/datastructures/binary_tree_node.cpp
@@ -5,18 +5,21 @@ struct binary_tree_node {
 	using pr = std::pair<node *, node *>;
 	node *l = nullptr, *r = nullptr, *p = nullptr;
 
+	const node *up() const { return static_cast<const node *>(this); }
+	const node *root() const { return this->p ? p->root() : up(); }
+
+	virtual node *u() { return up(); }
+	virtual node *n() { return up(); }
 	node *up() { return static_cast<node *>(this); }
-
 	node *root() { return this->p ? p->root() : up(); }
-	node *min() { return this->l ? l->min() : up(); }
-	node *max() { return this->r ? r->max() : up(); }
-	node *next() { return this->r ? this->r->min() : this->p; }
-	node *prev() { return this->l ? this->l->max() : this->p; }
+	node *min() { return this->n()->l ? l->min() : up(); }
+	node *max() { return this->n()->r ? r->max() : up(); }
+	node *next() { return this->n()->r ? this->r->min() : this->p; }
+	node *prev() { return this->n()->l ? this->l->max() : this->p; }
 
-	bool isroot() { return p == nullptr; }
-	bool isroot2() { return p == nullptr || (p->l != this && p->r != this); }
-	bool isleft() { return this == p->l; }
-	bool isright() { return this == p->r; }
+	bool isroot() const { return p == nullptr; }
+	bool isleft() const { return this == p->l; }
+	bool isright() const { return this == p->r; }
 
 	member get_member(bool right) {
 		return right ? &binary_tree_node::r : &binary_tree_node::l;
@@ -24,17 +27,20 @@ struct binary_tree_node {
 	node *child(bool right) { return this->*get_member(right); }
 
 	node *link(member m, node *c) { // return this
+		n();
 		this->*m = c;
-		if(c) c->p = up();
-		return up();
+		if(c != nullptr) c->p = up();
+		return this->u();
 	}
 	node *link(bool right, node *c) { return link(get_member(right), c); }
 	node *left(node *c) { return link(false, c); }
 	node *right(node *c) { return link(true, c); }
 
 	node *unlink(member m) { // return removed child
+		n();
 		node *c = this->*m;
 		if(c) this->*m = c->p = nullptr;
+		this->u();
 		return c;
 	}
 	node *unlink(bool right) { return unlink(get_member(right)); }
--- snippets/datastructures/splay_tree.cpp
+++ snippets/datastructures/splay_tree.cpp
@@ -1,38 +1,46 @@
 #include "../header.h"
 #include "./binary_tree_node.cpp"
-template <typename T>
-struct splay_tree_node : binary_tree_node<splay_tree_node<T>> {
-	using node = splay_tree_node;
+#include "tcr/utils/log_scope.cpp"
+template <typename node, typename T>
+struct splay_tree_node : binary_tree_node<node> {
 	using node_pr = std::pair<node *, node *>;
 	using binary_tree_node<node>::isleft;
 	T val;
 	splay_tree_node(const T &t = {}) : val(t) {}
+	node *up() { return static_cast<node *>(this); }
+	static T *get(node *x) { return x == nullptr ? nullptr : &x->val; }
+	virtual node *u() { return up(); }
+	virtual node *n() { return up(); }
 	node *rotate(bool right) { // return root of rotation
-		node *p = this, *x = p->child(!right);
-		assert(x);
+		node *p = up(), *x = p->child(!right);
 		x->p = p->p;
 		p->link(!right, x->child(right));
 		x->link(right, p);
 		if(x->p) (x->p->l == p ? x->p->l : x->p->r) = x;
+		p->u();
+		x->u();
 		return x;
 	}
 	node *splay() { // return new root
-		node *x = this;
-		if(x->isroot()) return x;
-		if(x->p->isroot()) return x->p->rotate(x->isleft());
-		if(x->isleft() == x->p->isleft())
-			return x->p->p->rotate(isleft())->rotate(isleft())->splay();
-		if(x->isleft() != x->p->isleft())
-			return x->p->rotate(isleft())->p->rotate(!isleft())->splay();
+		auto x = up();
+		if(x->isroot()) return x->n();
+		x->p->n();
+		x->n();
+		if(x->p->isroot()) return x->p->rotate(x->isleft())->n();
+		bool dir;
+		if((dir = x->isleft()) == x->p->isleft())
+			return x->p->p->rotate(dir)->rotate(dir)->splay();
+		if((dir = x->isleft()) != x->p->isleft())
+			return x->p->rotate(dir)->p->rotate(!dir)->splay();
 	}
 	static node *merge(node *s, node *t) { // s++t, both roots
 		if(s == nullptr) return t;
-		return s->max()->splay()->right(t);
+		return s->max()->splay()->right(t)->update();
 	}
 	template <typename... Ns>
 	static node *merge(node *l, Ns... ns) {
 		return merge(l, merge(ns...));
 	}
-	node_pr split() { return {splay()->unleft(), this}; }      // ..|x..
-	node_pr splitleft() { return {this, splay()->unright()}; } // ..x|..
+	node_pr split() { return {splay()->unleft()->u(), up()}; }      // ..|x..
+	node_pr splitleft() { return {up(), splay()->unright()->u()}; } // ..x|..
 };
