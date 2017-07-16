68b153132576b8eb4faa3839e623858578c46402
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2017-07-16 02:36:50 +0100

    TLE (working?) version of treap based link cut tree

---
+++ snippets/datastructures/link_cut_tree_treap.cpp
@@ -0,0 +1,125 @@
+#include "../header.h"
+#include "./sequence_link_cut_tree.cpp"
+struct link_cut_forest {
+	struct node;
+	struct T {
+		size_t v;
+		// int x = 0, sx = 0;	// for sum aggregate
+		node *pp = nullptr;
+		bool reverse = false;
+		void toggle() { reverse = !reverse; }
+	};
+	// void update(int u, int count) { nodes[u].access()->val.x = count; }
+	struct node : seq<node, T> {
+		using seq::seq;
+		node *access(bool lca = false) { // set for lca queries
+			if(auto r = this->split(false).second) r->val.pp = this;
+			node *last = this;
+			while(node *w = this->val.pp) {
+				last = w;
+				this->val.pp = nullptr;
+				if(auto r = w->split(false).second) r->val.pp = w;
+				node::merge(w, this);
+			}
+			if(lca) return last;
+			return this;
+		}
+		node *lc_root() { return access()->root()->min() /*->splay()*/; }
+		node *parent() {
+			node *previous = this->prev();
+			if(previous) return previous;
+			return this->root()->val.pp;
+		}
+		virtual node *u() {
+			if(val.pp == nullptr) {
+				if(l && l->val.pp) swap(val.pp, l->val.pp);
+				if(r && r->val.pp) swap(val.pp, r->val.pp);
+			}
+			return this;
+			/*
+			val.sx = val.x;
+			if(l) val.sx += l->val.sx;
+			if(r) val.sx += r->val.sx;
+			*/
+		}
+		virtual node *n() {
+			if(this->val.reverse) {
+				swap(this->l, this->r);
+				if(this->l) this->l->val.toggle();
+				if(this->r) this->r->val.toggle();
+				this->val.reverse = false;
+			}
+			return this;
+		}
+
+		friend ostream &operator<<(ostream &o, const node &x) {
+			o << '(';
+			if(x.l)
+				o << *x.l;
+			else
+				o << '-';
+			o << x.val.v;
+			// o<< (x.val.reverse ? 'R' : '.') << x.val.x << x.val.sx;
+			if(x.r)
+				o << *x.r;
+			else
+				o << '-';
+			o << ")";
+			if(x.isroot()) {
+				o << " ->> ";
+				if(x.val.pp)
+					o << x.val.pp->val.v;
+				else
+					o << '-';
+			} else {
+				if(x.val.pp) o << '*';
+			}
+			return o;
+		}
+	};
+	vector<node> nodes;
+	link_cut_forest(int n = 0) : nodes(n) {
+		for(int i = 0; i < n; ++i) nodes[i].val.v = i;
+	}
+	int add_vertex() {
+		nodes.push_back(T{nodes.size()});
+		return nodes.size() - 1;
+	}
+	node *reroot(node *x) {
+		x->access()->root()->val.toggle();
+		return x;
+	}
+	void cut(node *x) { x->access()->split(); }
+	void cut(int v) { cut(&nodes[v]); } // cut v from parent
+	void cut(int u, int v) {
+		if(&nodes[v] == nodes[u].parent())
+			cut(&nodes[u]);
+		else
+			cut(&nodes[v]);
+	}
+	bool connected(int u, int v) {
+		return nodes[u].lc_root() == nodes[v].lc_root();
+	}
+	void attach(int u, int v) { // u must be root; make child of v
+		// assert(nodes[u].l == nullptr);
+		nodes[u].access()->left(nodes[v].access()->root());
+	}
+	void link(node *x, node *y) { // will reroot x
+		reroot(x)->access()->left(y->access()->root());
+	}
+	void link(int u, int v) { link(&nodes[u], &nodes[v]); } // u child of v
+	int lca(int u, int v) {
+		nodes[u].access();
+		return nodes[v].access(true) - &nodes[0];
+	}
+};
+ostream &operator<<(ostream &o, const link_cut_forest::node *x) {
+	if(x) return o << *x;
+	return o << '-';
+}
+
+// todo: use the internal splay tree pointer for path parent
+// http://codeforces.com/contest/117/submission/860934
+// http://www.spoj.com/problems/DYNACON1/
+// http://www.spoj.com/problems/DYNACON2/
+// http://www.spoj.com/problems/DYNALCA/
---
+++ snippets/datastructures/sequence_link_cut_tree.cpp
@@ -0,0 +1,60 @@
+#include "../header.h"
+#include "./binary_tree_node.cpp"
+template <typename node, typename T>
+struct seq : binary_tree_node<node> {
+	using np = pair<node *, node *>;
+	T val;
+	int priority;
+	seq(T _v = {}) : val(_v) { priority = rand(); }
+	virtual node *u() { return this->up(); } // override for update
+	virtual node *n() { return this->up(); } // override for normalize
+	node *update() {
+		if(this->l) this->l->p = this->up();
+		if(this->r) this->r->p = this->up();
+		return u();
+	}
+	np split(bool right = true) {
+		auto _l = this->get_member(!right), _r = this->get_member(right);
+		node *A = this->up();
+		if(A == nullptr) return {nullptr, nullptr};
+		node *B = A, *lr = A;
+		A = A->*_l;
+		if(A == nullptr && lr->p == nullptr) {
+			return right ? np{nullptr, B} : np{B, nullptr};
+		}
+		if(A == nullptr) {
+			while(lr->p && lr->p->*_l == B) lr = B = lr->p;
+			if(lr->p) {
+				lr = A = lr->p;
+				lr->unlink(right);
+			}
+		} else
+			lr->unlink(!right);
+		while(lr->update()->p) {
+			if(lr->p->*_l == lr) {
+				if(lr == A) swap(A->p, B->p), B->p->*_l = B;
+				lr = B = B->p;
+			} else {
+				if(lr == B) swap(A->p, B->p), A->p->*_r = A;
+				lr = A = A->p;
+			}
+		}
+		return np{A, B};
+	}
+	// Note: Assumes both nodes are the roots of their sequences.
+	static node *merge(node *A, node *B) {
+		if(A == nullptr) return B;
+		if(B == nullptr) return A;
+		if(A->priority > B->priority) {
+			A->r = merge(A->r, B);
+			return A->update();
+		} else {
+			B->l = merge(A, B->l);
+			return B->update();
+		}
+	}
+	template <typename... Ns>
+	node *merge(node *l, Ns... ns) {
+		return merge(l, merge(ns...));
+	}
+};
