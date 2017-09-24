b39845cbd5ff3eb9652b0910a38f6d9863b89361
Timon Knigge <t.e.knigge@gmail.com>
2017-09-24 12:02:54 +0200

    SegmentTree: move identity element from template to constructor - only fundamental types can be template arguments (this makes it easier to make e.g. a SegmentTree over pairs).

--- snippets/datastructures/segmenttree.cpp
+++ snippets/datastructures/segmenttree.cpp
@@ -1,14 +1,14 @@
 #include "../header.h"
-template <class T, const T&(*op)(const T&, const T&), T ident>
+template <class T, const T&(*op)(const T&, const T&)>
 struct SegmentTree {
-	int n; vector<T> tree;
-	SegmentTree(int _n) : n(_n), tree(2 * n, ident) { }
+	int n; vector<T> tree; T id;
+	SegmentTree(int _n, T _id) : n(_n), tree(2 * n, _id), id(_id) { }
 	void update(int i, T val) {
 		for (tree[i+n] = val, i = (i+n)/2; i > 0; i /= 2)
 			tree[i] = op(tree[2*i], tree[2*i+1]);
 	}
 	T query(int l, int r) {
-		T lhs = T(ident), rhs = T(ident);
+		T lhs = T(_id), rhs = T(_id);
 		for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
 			if (  l&1 ) lhs = op(lhs, tree[l++]);
 			if (!(r&1)) rhs = op(tree[r--], rhs);
