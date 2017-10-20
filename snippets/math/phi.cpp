3ae703af61bb0979d3656a14a97c16663e0d1a70
Timon Knigge <t.e.knigge@gmail.com>
2017-10-20 14:35:09 +0200

    Clean up Euler-Phi calculation a bit.

--- snippets/math/phi.cpp
+++ snippets/math/phi.cpp
@@ -1,7 +1,10 @@
 #include "../header.h"
-void calculate_phi(int n, vector<ll> &phi) {
-	phi.resize(n);
-	iota(phi.begin(), phi.end(), 0); // numeric
-	for (ll i=2; i<=n; ++i) if (phi[i] == i)
-		for (ll j=i; j<=n; j+=i) phi[j] -= phi[j]/i;
+vi calculate_phi(int n) {
+	vi phi(n + 1, 0LL);
+	iota(phi.begin(), phi.end(), 0LL);
+	for (ll i = 2LL; i <= n; ++i)
+		if (phi[i] == i)
+			for (ll j = i; j <= n; j += i)
+				phi[j] -= phi[j] / i;
+	return phi;
 }
