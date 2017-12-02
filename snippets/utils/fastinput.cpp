85eb25b81dc5104a71657d16bb7d913df2847332
Timon Knigge <t.e.knigge@gmail.com>
2017-12-02 23:52:35 +0100

    Bugfix in fast IO.

--- snippets/utils/fastinput.cpp
+++ snippets/utils/fastinput.cpp
@@ -2,13 +2,14 @@
 int r() {
 	int sign = 1, n = 0;
 	char c;
-	while ((c = getchar_unlocked()) != '\n')
+	while (c = getchar_unlocked())
 		switch (c) {
 			case '-': sign = -1; break;
 			case ' ': case '\n': return n * sign;
 			default: n *= 10; n += c - '0'; break;
 		}
 }
+// Don't forget newlines!
 void print(ll x){
 	char buf[20], *p=buf;
 	if(!x) putchar_unlocked('0');
