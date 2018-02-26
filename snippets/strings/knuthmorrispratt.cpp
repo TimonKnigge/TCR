c2bab8c4b0db8391b43f5448d1594e1b9fdbd716
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2018-02-27 00:25:55 +0100

    formatting change

--- snippets/strings/knuthmorrispratt.cpp
+++ snippets/strings/knuthmorrispratt.cpp
@@ -3,23 +3,23 @@ void compute_prefix_function(string &w, vi &pi) {
 	pi.assign(w.length(), 0);
 	int k = pi[0] = -1;
 
-	for (int i = 1; i < w.length(); ++i) {
-		while (k >= 0 && w[k + 1] != w[i])
-			k = pi[k];
-		if (w[k + 1] == w[i]) k++;
+	for(int i = 1; i < w.length(); ++i) {
+		while(k >= 0 && w[k + 1] != w[i]) k = pi[k];
+		if(w[k + 1] == w[i]) k++;
 		pi[i] = k;
 	}
 }
 
 void knuth_morris_pratt(string &s, string &w) {
-	int q = -1; vi pi;
+	int q = -1;
+	vi pi;
 	compute_prefix_function(w, pi);
-	for (int i = 0; i < s.length(); ++i) {
-		 while (q >= 0 && w[q + 1] != s[i]) q = pi[q];
-		 if (w[q + 1] == s[i]) q++;
-		 if (q + 1 == w.length()) {
-			  // Match at position (i - w.length() + 1)
-			  q = pi[q];
-		 }
+	for(int i = 0; i < s.length(); ++i) {
+		while(q >= 0 && w[q + 1] != s[i]) q = pi[q];
+		if(w[q + 1] == s[i]) q++;
+		if(q + 1 == w.length()) {
+			// Match at position (i - w.length() + 1)
+			q = pi[q];
+		}
 	}
 }
