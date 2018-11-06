3b50abc35d808e91d7887a5688cc4a442b736a7c
Timon Knigge <t.e.knigge@gmail.com>
2018-11-06 12:58:36 +0100

    Add hashtable.

--- snippets/datastructures/builtin.cpp
+++ snippets/datastructures/builtin.cpp
@@ -15,3 +15,19 @@ using order_tree = tree<
 // find_by_order(int r) (0-based)
 // order_of_key(TIn v)
 // use key pair<Tin,int> {value, counter} for multiset/multimap
+
+// Need a much better hash function for gp_hash_table
+struct custom_hash {
+	static uint64_t splitmix64(uint64_t x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31); }
+	size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM
+			= chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM); } };
+
+template<class TIn, class TOut>
+using table = gp_hash_table<TIn, TOut>;
+// Or gp_hash_table<uint64_t, TOut, custom_hash>
