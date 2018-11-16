1961cfb81c238837438a646790e8bd0f4c268420
Timon Knigge <t.e.knigge@gmail.com>
2018-11-16 13:44:06 +0100

    Revert "make fenwick tree 0-based and right-exclusive"
    This reverts commit b8df1f470e4b0158318daf9c85c6587b7b177269.

--- snippets/datastructures/fenwick.cpp
+++ snippets/datastructures/fenwick.cpp
@@ -1,16 +1,15 @@
 #include "../header.h"
 template <class T>
-struct FenwickTree { // queries are right-exclusive; 0-based
-	int n;
-	vector<T> tree;
+struct FenwickTree {		// use 1 based indices!!!
+	int n; vector<T> tree;
 	FenwickTree(int n) : n(n) { tree.assign(n + 1, 0); }
-	T query(int l, int r) { return query(r) - query(l); } // [l,r)
-	T query(int r) {                                      // [0,r)
+	T query(int l, int r) { return query(r) - query(l - 1); }
+	T query(int r) {
 		T s = 0;
 		for(; r > 0; r -= (r & (-r))) s += tree[r];
 		return s;
 	}
 	void update(int i, T v) {
-		for(++i; i <= n; i += (i & (-i))) tree[i] += v;
+		for(; i <= n; i += (i & (-i))) tree[i] += v;
 	}
 };
