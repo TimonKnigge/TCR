f46517e6cd8377c51358b6f8e5c6e6496c1b1e3b
Timon Knigge <t.e.knigge@gmail.com>
2018-11-16 23:11:28 +0100

    Add root precomputation to FFT.

--- snippets/math/fft.cpp
+++ snippets/math/fft.cpp
@@ -1,8 +1,8 @@
 #include "../header.h"
 #include "./complex.cpp"
 #include "./field.cpp"
-ll brinc(ll x, ll k) {
-	ll i = k - 1, s = 1 << i;
+void brinc(int &x, int k) {
+	int i = k - 1, s = 1 << i;
 	x ^= s;
 	if ((x & s) != s) {
 		--i; s >>= 1;
@@ -10,18 +10,30 @@ ll brinc(ll x, ll k) {
 			x = x &~ s, --i, s >>= 1;
 		if (i >= 0) x |= s;
 	}
-	return x;
 }
 using T = Complex;	// using T=F1,F2,F3
+vector<T> roots;
+void root_cache(int N) {
+	if (N == (int)roots.size()) return;
+	roots.assign(N, T{0});
+	for (int i = 0; i < N; ++i)
+		roots[i] = ((i&-i) == i)
+			? T{cos(2.0*pi*i/N), sin(2.0*pi*i/N)}
+			: roots[i&-i] * roots[i-(i&-i)];
+}
 void fft(vector<T> &A, int p, bool inv = false) {
 	int N = 1<<p;
-	for(int i = 0, r = 0; i < N; ++i, r = brinc(r, p))
+	for(int i = 0, r = 0; i < N; ++i, brinc(r, p))
 		if (i < r) swap(A[i], A[r]);
+//	Uncomment to precompute roots (for T=Complex). Slower but more precise.
+//	root_cache(N);
+//				, sh=p-1		, --sh
 	for (int m = 2; m <= N; m <<= 1) {
 		T w, w_m = T::root(inv ? -m : m);
 		for (int k = 0; k < N; k += m) {
 			w = T{1};
 			for (int j = 0; j < m/2; ++j) {
+//				T w = (!inv ? roots[j<<sh] : roots[j<<sh].conj());
 				T t = w * A[k + j + m/2];
 				A[k + j + m/2] = A[k + j] - t;
 				A[k + j] = A[k + j] + t;
