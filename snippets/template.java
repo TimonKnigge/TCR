1a0f471058448e42ce87b63a1038e5e3bac2a7a9
Timon Knigge <t.e.knigge@gmail.com>
2018-11-24 17:38:07 +0100

    Fix template.

--- snippets/template.java
+++ snippets/template.java
@@ -1,15 +1,7 @@
 // lstlistings skips the first [this] line
-import java.io.OutputStream;
-import java.io.InputStream;
-import java.io.PrintWriter;
-import java.util.StringTokenizer;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.InputStream;
-import java.io.IOException;
-
-import java.util.Arrays;
-import java.math.BigInteger;
+import java.io.*;
+import java.util.*;
+import java.math.*;
 
 public class Main { // Check what this should be called
 	public static void main(String[] args) {
