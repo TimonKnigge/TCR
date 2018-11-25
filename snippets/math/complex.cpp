0fd218d7e89009efb317617421fd910ca4ab8dcb
Timon Knigge <t.e.knigge@gmail.com>
2018-11-25 13:33:18 +0100

    Add HP Convolution.

--- snippets/math/complex.cpp
+++ snippets/math/complex.cpp
@@ -1,7 +1,7 @@
 #include "../header.h"
-constexpr ld pi = 3.1415926535897932384626433;
-struct Complex { using T = Complex; ld u,v;
-	Complex(ld u=0, ld v=0) : u{u}, v{v} {}
+constexpr double pi = 3.1415926535897932384626433; // or std::acos(-1)
+struct Complex { using T = Complex; double u,v;
+	Complex(double u=0, double v=0) : u{u}, v{v} {}
 	T operator+(T r) const { return {u+r.u, v+r.v}; }
 	T operator-(T r) const { return {u-r.u, v-r.v}; }
 	T operator*(T r) const { return {u*r.u - v*r.v, u*r.v + v*r.u}; }
@@ -9,7 +9,10 @@ struct Complex { using T = Complex; ld u,v;
 		auto norm = r.u*r.u+r.v*r.v;
 		return {(u*r.u + v*r.v)/norm, (v*r.u - u*r.v)/norm};
 	}
+	T operator*(double r) const { return T{u*r, v*r}; }
+	T operator/(double r) const { return T{u/r, v/r}; }
 	T inv() const { return T{1,0}/ *this; }
+	T conj() const { return T{u, -v}; }
 	static T root(ll k){ return {cos(2*pi/k), sin(2*pi/k)}; }
 	bool zero() const { return max(abs(u), abs(v)) < 1e-6; }
 };
---
+++ snippets/math/high-prec-convolution.cpp
@@ -0,0 +1,23 @@
+#include "../header.h"
+#include "./fft.cpp"
+void convolution_mod(const vi &A, const vi &B, ll MOD, vi &C) {
+	int s = A.size() + B.size() - 1; ll m15 = (1LL<<15)-1LL;
+	int q = 32 - __builtin_clz(s-1), N=1<<q;	// fails if s=1
+	vector<T> Ac(N), Bc(N), R1(N), R2(N);
+	for (size_t i = 0; i < A.size(); ++i) Ac[i] = T{A[i]&m15, A[i]>>15};
+	for (size_t i = 0; i < B.size(); ++i) Bc[i] = T{B[i]&m15, B[i]>>15};
+	fft(Ac, q, false); fft(Bc, q, false);
+	for (int i = 0, j = 0; i < N; ++i, j = (N-1)&(N-i)) {
+		T as = (Ac[i] + Ac[j].conj()) / 2;
+		T al = (Ac[i] - Ac[j].conj()) / T{0, 2};
+		T bs = (Bc[i] + Bc[j].conj()) / 2;
+		T bl = (Bc[i] - Bc[j].conj()) / T{0, 2};
+		R1[i] = as*bs + al*bl*T{0,1}, R2[i] = as*bl + al*bs;
+	}
+	fft(R1, q, true); fft(R2, q, true);
+	C.resize(s);
+	for (int i = 0; i < s; ++i) {
+		ll l = llround(R1[i].u), m = llround(R2[i].u), h = llround(R1[i].v);
+		C[i] = (l + (m%MOD<<15) + (h%MOD<<30)) % MOD;
+	}
+}
