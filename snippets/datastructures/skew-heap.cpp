74c3fd1b72d0ce0cffde758160da46aa43634f1a
Timon Knigge <t.e.knigge@gmail.com>
2018-11-26 20:07:39 +0100

    Add lazily propagated weight adjusting to the skew heap.

--- snippets/datastructures/skew-heap.cpp
+++ snippets/datastructures/skew-heap.cpp
@@ -2,26 +2,34 @@
 template <class T>
 struct SkewHeap {
 	struct node {
-		ll key; T val;
+		ll key, lazy = 0LL; T val;
 		node *lc = nullptr, *rc = nullptr;
 		node(ll k, T v) : key(k), val(v) {}
+		node *prop() {
+			if (lc) lc->lazy += lazy;
+			if (rc) rc->lazy += lazy;
+			key += lazy, lazy = 0LL;
+			return this;
+		}
 	};
 	node *r = nullptr;
 	node* merge(node* x, node* y){
 		if (!x) return y; else if (!y) return x;
-		if (x->key > y->key) swap(x, y);
+		if (x->prop()->key > y->prop()->key) swap(x, y);
 		x->rc = merge(x->rc, y);
 		swap(x->lc, x->rc);
 		return x;
 	}
 
-	bool empty() { return r == nullptr; }
+	bool empty() { return r != nullptr; }
 	void insert(ll x, T val) { r = merge(r, new node(x, val)); }
-	T pop_min() {
+	void adjust(ll c) { if (r) r->lazy += c; }
+	pair<ll, T> pop_min() {
+		ll w = r->prop()->key;
 		T ret = r->val;
 		node *nr = merge(r->lc, r->rc);
 		swap(r, nr), delete nr;
-		return ret;
+		return {w, ret};
 	}
 	void absorb(SkewHeap<T> &o) { r = merge(r, o.r); o.r = nullptr; }
 };
