98521565c688579e767a58b2880042b090915fa8
Timon Knigge <t.e.knigge@gmail.com>
2018-11-29 22:06:36 +0100

    Shorten dominator set.

--- snippets/datastructures/dominating-set.cpp
+++ snippets/datastructures/dominating-set.cpp
@@ -1,17 +1,18 @@
 #include "../header.h"
-struct dominating_set {
+struct dominator_set {
 	map<ll, ll> m;
 	void insert(ll a, ll b) {
 		auto it = m.lower_bound(a);
 		if (it != m.end() && it->second >= b)
 			return;
-		while (!m.empty()) {
-			if ((it = m.upper_bound(a)) != m.begin()) {
-				--it;
-				if (it->first <= a && it->second <= b)
-					m.erase(it); else break;
-			} else break;
-		}
+		while (!m.empty() && (it = m.upper_bound(a)) != m.begin())
+			if ((--it)->first <= a && it->second <= b)
+				m.erase(it); else break;
 		m[a] = b;
+	}
+	// max { b | (a, b) \in m, a >= u }, or -LLINF otherwise.
+	ll max_tail(ll u) {
+		auto it = m.lower_bound(u);
+		return (it != m.end() ? it->second : -LLINF);
 	}
 };
