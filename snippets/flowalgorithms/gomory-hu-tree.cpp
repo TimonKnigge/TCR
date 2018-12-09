0e715b3392acf0f95e2277b2abc07a4dc5393725
Timon Knigge <t.e.knigge@gmail.com>
2018-12-09 01:15:38 +0100

    Clean up Gomory Hu Tree a bit.

--- snippets/flowalgorithms/gomory-hu-tree.cpp
+++ snippets/flowalgorithms/gomory-hu-tree.cpp
@@ -2,20 +2,17 @@
 #include "./dinic.cpp"
 struct edge { int u, v; ll w; };
 struct GomoryHuTree {		// After construction, for all n-1 i such that
-	int V;					// p[i] \geq 0, the edge (i, p[i]) is in the GH
-	vi p, w, c;				// tree with weight w[i].
-	vector<edge> tree;
+	int V; vi p, w, c;		// p[i] \geq 0, the edge (i, p[i]) is in the GH
+	vector<edge> tree;		// tree with weight w[i].
 	void dfs(int u, const FlowGraph &fg) {
 		c[u] = 1;
-		for (const auto &e : fg[u])
-			if (!c[e.v] && e.res())
-				dfs(e.v, fg);
+		for (const auto &e : fg[u]) if (!c[e.v] && e.res()) dfs(e.v, fg);
 	}
 	GomoryHuTree(int n, const vector<edge> &E) : V(n), p(V), w(V), c(V) {
 		FlowGraph fg(V);
 		for (const edge &e : E) fg.add_edge(e.u, e.v, e.w);
 		p[0] = -1, std::fill(p.begin() + 1, p.end(), 0);
-		for (int i = 1; i < V; ++i) {
+		for (int i = 1; i < V; ++i, fg.clear()) {
 			w[i] = Dinic(fg, i, p[i]).run();
 			std::fill(c.begin(), c.end(), 0);
 			dfs(i, fg);
@@ -24,10 +21,8 @@ struct GomoryHuTree {		// After construction, for all n-1 i such that
 			if (p[p[i]] >= 0 && c[p[p[i]]]) {
 				int pi = p[i];
 				swap(w[i], w[pi]);
-				p[i] = p[pi];
-				p[pi] = i;
+				p[i] = p[pi], p[pi] = i;
 			}
-			fg.clear();
 		}
 	}
 };
