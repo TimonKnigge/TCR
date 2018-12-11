64eb1a2da9cd9fd1b4e1b849e8e14a16022e96fc
Timon Knigge <t.e.knigge@gmail.com>
2018-12-11 13:11:03 +0100

    snippets/flowalgorithms/dinic.cpp

--- snippets/flowalgorithms/dinic.cpp
+++ snippets/flowalgorithms/dinic.cpp
@@ -6,14 +6,15 @@ struct Dinic{
 	Dinic(FlowGraph &edges, int s, int t) :
 		edges(edges), V(edges.size()), s(s), t(t), l(V,-1), its(V) {}
 	ll augment(int u, F c) { // we reuse the same iterators
-		if (u == t) return c;
+		if (u == t) return c; ll r = 0LL;
 		for(auto &i = its[u]; i != edges[u].end(); i++){
 			auto &e = *i;
 			if (e.cap > e.f && l[u] < l[e.v]) {
 				auto d = augment(e.v, min(c, e.cap - e.f));
-				if (d > 0) { e.f += d; edges[e.v][e.r].f -= d; return d; }
-			}	}
-		return 0;
+				if (d > 0) { e.f += d; edges[e.v][e.r].f -= d; c -= d;
+					r += d; if (!c) break; }
+		}	}	}
+		return r;
 	}
 	ll run() {
 		ll flow = 0, f;
@@ -21,12 +22,11 @@ struct Dinic{
 			fill(l.begin(), l.end(),-1); l[s]=0; // recalculate the layers
 			queue<int> q; q.push(s);
 			while(!q.empty()){
-				auto u = q.front(); q.pop();
+				auto u = q.front(); q.pop(); its[u] = edges[u].begin();
 				for(auto &&e : edges[u]) if(e.cap > e.f && l[e.v]<0)
 					l[e.v] = l[u]+1, q.push(e.v);
 			}
 			if (l[t] < 0) return flow;
-			for (int u = 0; u < V; ++u) its[u] = edges[u].begin();
 			while ((f = augment(s, INF)) > 0) flow += f;
 		}	}
 };
