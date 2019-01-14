793d06bf5065852b9a56da1593ded5f7d1454aec
Timon Knigge <t.e.knigge@gmail.com>
2019-01-14 21:58:13 +0100

    Much faster and more compact implementation of Lucy's algorithm.

--- snippets/math/lucy.cpp
+++ snippets/math/lucy.cpp
@@ -1,24 +1,16 @@
 #include "../header.h"
-ll f(ll n) { return n*n; }
-ll F(ll n) { return n*(n+1)*(2*n+1)/6LL; }
-ll lucy(ll n) {
-	ll k = ll(sqrt(n));
-	vi V;
-	for (ll i = 1; i <= k; ++i) V.push_back(i), V.push_back(n/i);
-	sort(V.rbegin(), V.rend());
-	V.erase(unique(V.begin(), V.end()), V.end());
-
-	unordered_map<ll, ll> S;
-	for (size_t i = 0; i < V.size(); ++i)
-		S[V[i]] = F(V[i]) - F(1);
-
-	for (ll p = 2LL; p <= k; ++p)
-		if (S[p] > S[p-1]) {
-			ll sp = S[p-1];
-			for (ll v : V)
-				if (v >= p*p)
-					S[v] -= f(p)*(S[v/p] - sp);
-				else break;
-		}
-	return S[n];
+ll f(ll n) { return n; }
+ll F(ll n) { return n*(n+1)/2LL; } // = \sum_{i \leq n} f(i)
+size_t id(ll i, ll N, const vi &V) {
+	return i <= V.size()/2 ? V.size()-i : N/i-1;
+}	// When adding 0 to V, add a -1 ^ here.
+ll lucy(ll N) {
+	vi V, S;
+	for (ll i = 1; i <= N; i = N/(N/i)+1)
+		V.push_back(N/i), S.push_back(F(N/i)-F(1));
+	for (ll p = 2LL, sp; p*p <= N; ++p)
+		if (S[id(p, N, V)] > (sp = S[id(p-1, N, V)]))
+			for (size_t i = 0; i < V.size() && V[i] >= p*p; ++i)
+				S[i] -= f(p)*(S[id(V[i]/p, N, V)] - sp); // <= UPDATE
+	return S[0]; // Or return all of S for summation upto all N/k.
 }
